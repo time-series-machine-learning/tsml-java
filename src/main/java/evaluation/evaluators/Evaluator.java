613a78aaf998af96bdaae442444b9b7c5121a180
James Large <james.large@uea.ac.uk>
2019-02-26 10:58:40 +0000

    gnu license spammed across the evaluator package

--- src/main/java/evaluation/evaluators/Evaluator.java
+++ src/main/java/evaluation/evaluators/Evaluator.java
@@ -1,3 +1,17 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package evaluation.evaluators;
 
 import evaluation.storage.ClassifierResults;
@@ -6,7 +20,7 @@ import weka.core.Instances;
 
 /**
  *
- * @author xmw13bzu
+ * @author James Large (james.large@uea.ac.uk)
  */
 public abstract class Evaluator {
     int seed;
@@ -44,18 +58,48 @@ public abstract class Evaluator {
         this.seed = seed;
     }
 
+    /**
+     * Flag for whether to clone the data. Defaults to false, as no classifier should 
+     * be editing the data itself when training/testing, however setting this to true 
+     * will guarantee that the same (jave) instantiations of (weka) instance(s) objects
+     * can be reused in higher-level experimental code. 
+     */
     public boolean getCloneData() {
         return cloneData;
     }
 
+    /**
+     * Flag for whether to clone the data. Defaults to false, as no classifier should 
+     * be editing the data itself when training/testing, however setting this to true 
+     * will guarantee that the same (jave) instantiations of (weka) instance(s) objects
+     * can be reused in higher-level experimental code. 
+     */
     public void setCloneData(boolean cloneData) {
         this.cloneData = cloneData;
     }
-
+    
+    /**
+     * Each instance will have setClassMissing() called upon it. To ABSOLUTELY enforce that 
+     * no classifier can cheat in any way (e.g some filter/transform inadvertently incorporates the class 
+     * value back into the transformed data set). 
+     * 
+     * The only reason to leave this as false (as it has been by default, for backwards compatability reasons)
+     * is that in higher level experimental code, the same (jave) instantiations of (weka) instance(s) objects are used multiple 
+     * times, and the latter expects the class value to still be there (to check for correct predictions, e.g)
+     */
     public boolean getSetClassMissing() {
         return setClassMissing;
     }
-
+    
+    /**
+     * Each instance will have setClassMissing() called upon it. To ABSOLUTELY enforce that 
+     * no classifier can cheat in any way (e.g some filter/transform inadvertently incorporates the class 
+     * value back into the transformed data set). 
+     * 
+     * The only reason to leave this as false (as it has been by default, for backwards compatability reasons)
+     * is that in higher level experimental code, the same (jave) instantiations of (weka) instance(s) objects are used multiple 
+     * times, and the latter expects the class value to still be there (to check for correct predictions, e.g)
+     */
     public void setSetClassMissing(boolean setClassMissing) {
         this.setClassMissing = setClassMissing;
     }
