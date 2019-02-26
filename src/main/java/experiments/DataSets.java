c276c2639331fbc7376c3271e2f3eb3bcb959216
James Large <james.large@uea.ac.uk>
2019-02-26 12:34:16 +0000

    experiments and fileio package licenses added

--- src/main/java/experiments/DataSets.java
+++ src/main/java/experiments/DataSets.java
@@ -1,5 +1,16 @@
 /*
- Class containing data sets used in various TSC papers
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package experiments;
 
@@ -29,7 +40,7 @@ import java.util.zip.ZipOutputStream;
 import weka.classifiers.Classifier;
 import weka.classifiers.lazy.IBk;
 /**
- *
+ * Class containing data sets used in various TSC papers
  * @author ajb
  */
 public class DataSets {
