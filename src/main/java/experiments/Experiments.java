f4e90f50fcb9b5170d09736762c770aeb33e6bd9
James Large <44509982+James-Large@users.noreply.github.com>
2019-06-24 15:07:42 +0100

    Update Experiments.java
    removed edit to experiments, again intended for paper/alcohol branch

--- src/main/java/experiments/Experiments.java
+++ src/main/java/experiments/Experiments.java
@@ -433,9 +433,7 @@ public class Experiments  {
         }
         else {           
 //            Classifier classifier = ClassifierLists.setClassifierClassic(expSettings.classifierName, expSettings.foldId);
-//            Classifier classifier = ClassifierLists.setClassifier(expSettings);
-            //jamesl paper/alcohol branch
-            Classifier classifier = AlcoholPaper.AlcoholClassifierList.setClassifier(expSettings);
+            Classifier classifier = ClassifierLists.setClassifier(expSettings);
             
             Instances[] data = sampleDataset(expSettings.dataReadLocation, expSettings.datasetName, expSettings.foldId);
         
