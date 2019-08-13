5ba5d6c5ca0d5aabe7e0b82b5c1a6507516d3490
George Oastler <g.oastler@uea.ac.uk>
2019-08-13 20:28:37 +0100

    filter utils

---
+++ src/main/java/utilities/FilterUtilities.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.filters.Filter;
+
+import java.util.Arrays;
+
+public class FilterUtilities {
+    public static Instances filter(Instances data, Filter filter) throws
+                                                                  Exception {
+        filter.setInputFormat(data);
+        return Filter.useFilter(data, filter);
+    }
+
+    public static Instance filter(Instance instance, Filter filter) throws
+                                                                    Exception {
+        Instances instances = toInstances(instance);
+        Instances filtered = filter(instances, filter);
+        return filtered.get(0);
+    }
+
+    public static Instances toInstances(Instance... instances) {
+        Instances result = new Instances(instances[0].dataset());
+        result.addAll(Arrays.asList(instances));
+        return result;
+    }
+}
