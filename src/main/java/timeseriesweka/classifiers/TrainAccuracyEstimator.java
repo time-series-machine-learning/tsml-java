cfa4e0bfed1f2ff9b232c5235d52560656cff269
George Oastler <g.oastler@uea.ac.uk>
2019-08-28 09:13:27 +0100

    Rogue exception

--- src/main/java/timeseriesweka/classifiers/TrainAccuracyEstimator.java
+++ src/main/java/timeseriesweka/classifiers/TrainAccuracyEstimator.java
@@ -56,7 +56,7 @@ public interface TrainAccuracyEstimator extends SaveParameterInfo{
 * 
  * @param train: Full file name for the TrainCV results
  */    
-    default void writeTrainEstimatesToFile(String train) throws Exception {
+    default void writeTrainEstimatesToFile(String train) {
         throw new UnsupportedOperationException();
     }
 /**
