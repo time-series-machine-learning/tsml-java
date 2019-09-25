7abdae9c8b89a7a394a7a3e005fbc646605e405b
James Large <james.large@uea.ac.uk>
2019-09-25 16:35:09 +0100

    atmpt1 gradle messing to get tests running on travis

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 }
 
 dependencies {
+    testCompile group: 'junit', name: 'junit', version: '4.12'
     testImplementation("junit:junit:4.12")
     implementation(fileTree("lib"))
 }
