44ae17a85e29b8e9f6154d5bf7e039ecc32a5c97
TonyBagnall <ajb@uea.ac.uk>
2019-10-21 12:57:48 +0100

    newline added

--- src/main/java/timeseriesweka/filters/shapelet_transforms/ShapeletTransform.java
+++ src/main/java/timeseriesweka/filters/shapelet_transforms/ShapeletTransform.java
@@ -1223,7 +1223,7 @@ public class ShapeletTransform extends SimpleBatchFilter implements Serializable
 
     @Override
     public String toString() {
-        String str = "Shapelets: ";
+        String str = "Shapelets: \n";
         for (Shapelet s : shapelets) {
             str += s.toString() + "\n";
         }
