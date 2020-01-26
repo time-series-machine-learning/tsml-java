d9b3f115ef9a29cca354ad3e8231d162fc7c17e0
George Oastler <goastler4@gmail.com>
2020-01-26 18:26:35 +0000

    logging

--- src/main/java/utilities/LogUtils.java
+++ src/main/java/utilities/LogUtils.java
@@ -1,7 +1,6 @@
 package utilities;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 public class LogUtils {
     private LogUtils() {}
@@ -9,7 +8,21 @@ public class LogUtils {
     public static Logger getLogger(Object object) {
         Logger logger = Logger.getLogger(object.getClass().getSimpleName() + "-" + object.hashCode());
         logger.setLevel(Level.OFF); // disable logs by default
+        logger.addHandler(buildStdErrStreamHandler(new SimpleFormatter()));
+        logger.addHandler(buildStdOutStreamHandler(new SimpleFormatter()));
         return logger;
+    }
+
+    public static StreamHandler buildStdErrStreamHandler(Formatter formatter) {
+        StreamHandler soh = new StreamHandler(System.out, formatter);
+        soh.setLevel(Level.ALL); //Default StdOut Setting
+        return soh;
+    }
+
+    public static StreamHandler buildStdOutStreamHandler(Formatter formatter) {
+        StreamHandler soh = new StreamHandler(System.out, formatter);
+        soh.setLevel(Level.SEVERE); //Default StdErr Setting
+        return soh;
     }
 
 }
