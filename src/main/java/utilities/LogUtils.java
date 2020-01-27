aabca06916a6ec62a99cd54c5a153eace0317e7a
George Oastler <goastler4@gmail.com>
2020-01-27 12:07:35 +0000

    Adjusted default logging setup

--- src/main/java/utilities/LogUtils.java
+++ src/main/java/utilities/LogUtils.java
@@ -6,22 +6,32 @@ public class LogUtils {
     private LogUtils() {}
 
     public static Logger getLogger(Object object) {
-        Logger logger = Logger.getLogger(object.getClass().getSimpleName() + "-" + object.hashCode());
+        String name = object.getClass().getSimpleName();
+        if(object instanceof Class) {
+            name = ((Class) object).getSimpleName();
+        } else {
+            name = object.getClass().getSimpleName() + "-" + object.hashCode();
+        }
+        Logger logger = Logger.getLogger(name);
+        Handler[] handlers = logger.getHandlers();
+        for(Handler handler : handlers) {
+            logger.removeHandler(handler);
+        }
         logger.setLevel(Level.OFF); // disable logs by default
-        logger.addHandler(buildStdErrStreamHandler(new SimpleFormatter()));
         logger.addHandler(buildStdOutStreamHandler(new SimpleFormatter()));
+        logger.addHandler(buildStdErrStreamHandler(new SimpleFormatter()));
         return logger;
     }
 
     public static StreamHandler buildStdErrStreamHandler(Formatter formatter) {
-        StreamHandler soh = new StreamHandler(System.out, formatter);
-        soh.setLevel(Level.ALL); //Default StdOut Setting
+        StreamHandler soh = new StreamHandler(System.err, formatter);
+        soh.setLevel(Level.SEVERE); //Default StdErr Setting
         return soh;
     }
 
     public static StreamHandler buildStdOutStreamHandler(Formatter formatter) {
         StreamHandler soh = new StreamHandler(System.out, formatter);
-        soh.setLevel(Level.SEVERE); //Default StdErr Setting
+        soh.setLevel(Level.ALL); //Default StdOut Setting
         return soh;
     }
 
