53d370b20c9e5fce921043a40d460a4f7d13a91c
George Oastler <goastler4@gmail.com>
2020-02-28 09:14:14 +0000

    DDTW refactor + commenting

--- src/main/java/tsml/classifiers/distance_based/distances/DDTWDistance.java
+++ src/main/java/tsml/classifiers/distance_based/distances/DDTWDistance.java
@@ -6,10 +6,15 @@ import weka.core.neighboursearch.PerformanceStats;
 
 import static tsml.filters.Derivative.INSTANCE_DERIVATIVE_FUNCTION;
 
-
+/**
+ * DDTW distance measure. This is the derivative version of DTW.
+ *
+ * Contributors: goastler
+ */
 public class DDTWDistance extends DTWDistance {
+    // Global derivative function which is cached, i.e. if you ask it to convert the same instance twice it will instead fetch from the cache the second time
     public static final CachedFunction<Instance, Instance> DERIVATIVE_CACHE = new CachedFunction<>(INSTANCE_DERIVATIVE_FUNCTION);
-
+    // Cache function for taking the derivative. I suggest setting these all to use the same cache if you've got multiple derivative based distance measures
     private CachedFunction<Instance, Instance> derivativeCache = DERIVATIVE_CACHE;
 
     public CachedFunction<Instance, Instance> getDerivativeCache() {
@@ -25,8 +30,10 @@ public class DDTWDistance extends DTWDistance {
                            final Instance second,
                            final double limit,
                            final PerformanceStats stats) {
+        // take the derivative of both instances
         Instance transformedFirst = derivativeCache.apply(first);
         Instance transformedSecond = derivativeCache.apply(second);
+        // find the DTW distance between them
         return super.distance(transformedFirst, transformedSecond, limit, stats);
     }
 
