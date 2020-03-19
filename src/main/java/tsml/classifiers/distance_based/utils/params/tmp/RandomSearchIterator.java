870d17e491bf1acf2f803dc6a97e004b313d118f
George Oastler <goastler4@gmail.com>
2020-03-19 16:27:09 +0000

    testing

--- src/main/java/tsml/classifiers/distance_based/utils/params/tmp/RandomSearchIterator.java
+++ src/main/java/tsml/classifiers/distance_based/utils/params/tmp/RandomSearchIterator.java
@@ -2,11 +2,10 @@ package tsml.classifiers.distance_based.utils.params.tmp;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import org.junit.Assert;
+import org.junit.Test;
 import tsml.classifiers.distance_based.utils.BaseRandom;
 import tsml.classifiers.distance_based.utils.params.ParamSet;
 import tsml.classifiers.distance_based.utils.params.distribution.Distribution;
@@ -54,20 +53,16 @@ public class RandomSearchIterator extends BaseRandom implements Iterator<ParamSe
         return iterationCount < numIterations;
     }
 
-    private Object extractRandomValue(ParameterDimension<?> dimension) {
+    private Object extractRandomValue(Object values) {
         Object value;
-        if(dimension instanceof ContinuousParameterDimension) {
-            ContinuousParameterDimension<?> continuousParameterDimension = (ContinuousParameterDimension<?>) dimension;
-            Distribution<?> distribution = continuousParameterDimension.getValues();
-            distribution.setRandom(getRandom());
-            value = distribution.sample();
-            distribution.setRandom(null);
-        } else if(dimension instanceof DiscreteParameterDimension) {
-            DiscreteParameterDimension<?> discreteParameterDimension = (DiscreteParameterDimension<?>) dimension;
-            List<?> values = discreteParameterDimension.getValues();
-            value = RandomUtils.choice(values, getRandom());
+        if(values instanceof Distribution<?>) {
+            Distribution<?> distribution = (Distribution<?>) values;
+            value = distribution.sample(getRandom());
+        } else if(values instanceof List<?>) {
+            List<?> list = (List<?>) values;
+            value = RandomUtils.choice(list, getRandom());
         } else {
-            throw new IllegalArgumentException("cannot handle type {" + dimension.getClass() + "} for dimension");
+            throw new IllegalArgumentException("cannot handle type {" + values.getClass() + "} for dimension content");
         }
         return value;
     }
@@ -78,7 +73,7 @@ public class RandomSearchIterator extends BaseRandom implements Iterator<ParamSe
         for(Map.Entry<String, List<ParameterDimension<?>>> entry : dimensionMap.entrySet()) {
             List<ParameterDimension<?>> dimensions = entry.getValue();
             ParameterDimension<?> dimension = RandomUtils.choice(dimensions, getRandom());
-            final Object value = extractRandomValue(dimension);
+            final Object value = extractRandomValue(dimension.getValues());
             final List<ParameterSpace> subSpaces = dimension.getSubSpaces();
             final List<ParamSet> subParamSets = new ArrayList<>();
             for(ParameterSpace subSpace : subSpaces) {
@@ -86,7 +81,7 @@ public class RandomSearchIterator extends BaseRandom implements Iterator<ParamSe
                 subParamSets.add(subParamSet);
             }
             final String name = entry.getKey();
-            paramSet.add(name,value, subParamSets);
+            paramSet.add(name, value, subParamSets);
         }
         return paramSet;
     }
@@ -106,5 +101,38 @@ public class RandomSearchIterator extends BaseRandom implements Iterator<ParamSe
         Assert.assertTrue(numIterations >= 0);
         this.numIterations = numIterations;
         return this;
+    }
+
+    public static class UnitTests {
+
+        @Test
+        public void testIteration() {
+            ParameterSpace space = ParameterSpace.UnitTests.build2DContinuousSpace();
+            final int limit = 10;
+            RandomSearchIterator iterator = new RandomSearchIterator(space, limit);
+            iterator.setRandom(ParameterSpace.UnitTests.buildRandom());
+            StringBuilder stringBuilder = new StringBuilder();
+            int count = 0;
+            while(iterator.hasNext()) {
+                count++;
+                ParamSet paramSet = iterator.next();
+                stringBuilder.append(paramSet);
+                stringBuilder.append("\n");
+            }
+            System.out.println(stringBuilder.toString());
+            Assert.assertEquals(count, limit);
+            Assert.assertEquals(stringBuilder.toString(),
+                "ParamSet{-a, \"0.4157204943935306\", -b, \"0.8187087126750541\"}\n"
+                    + "ParamSet{-a, \"0.058503304403612566\", -b, \"0.6666091997383249\"}\n"
+                    + "ParamSet{-a, \"0.3065178840223069\", -b, \"0.9395912589362401\"}\n"
+                    + "ParamSet{-a, \"0.08798840101774008\", -b, \"0.5644485754368884\"}\n"
+                    + "ParamSet{-a, \"0.35258737223772796\", -b, \"0.7733698785992328\"}\n"
+                    + "ParamSet{-a, \"0.2814748369491896\", -b, \"0.8125731817327797\"}\n"
+                    + "ParamSet{-a, \"0.00746354294055912\", -b, \"0.9953613928573914\"}\n"
+                    + "ParamSet{-a, \"0.43383933414698683\", -b, \"0.8665760350974969\"}\n"
+                    + "ParamSet{-a, \"0.006403325787859793\", -b, \"0.7633497173024331\"}\n"
+                    + "ParamSet{-a, \"0.49233707140341276\", -b, \"0.5415311991124574\"}\n"
+            );
+        }
     }
 }
