f67e6359adfb32e95e3e0e840dc41d28863b1863
George Oastler <goastler4@gmail.com>
2020-03-26 13:41:51 +0000

    convert name / keys to upper case

--- src/main/java/tsml/classifiers/distance_based/utils/classifier_building/ClassifierBuilderFactory.java
+++ src/main/java/tsml/classifiers/distance_based/utils/classifier_building/ClassifierBuilderFactory.java
@@ -1,68 +1,67 @@
 package tsml.classifiers.distance_based.utils.classifier_building;
 
+import com.beust.jcommander.internal.Lists;
 import com.google.common.collect.ImmutableSet;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import tsml.classifiers.EnhancedAbstractClassifier;
 import tsml.classifiers.distance_based.elastic_ensemble.ElasticEnsemble;
 import tsml.classifiers.distance_based.knn.KNN;
 import tsml.classifiers.distance_based.knn.KNNLOOCV;
 import weka.classifiers.Classifier;
 
-import java.util.*;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
 public class ClassifierBuilderFactory<B extends Classifier> {
-
-    public interface ClassifierBuilder<A extends Classifier> extends Supplier<A> {
-        String toString();
-        String getName();
-        A build();
-
-        @Override
-        default A get() {
-            return build();
-        }
-    }
-
-    public static class SuppliedClassifierBuilder<A extends Classifier> implements ClassifierBuilder<A> {
-        private final String name;
-        private final Supplier<? extends A> supplier;
-
-        public SuppliedClassifierBuilder(final String name, final Supplier<? extends A> supplier) {
-            this.name = name;
-            this.supplier = supplier;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public Supplier<? extends A> getSupplier() {
-            return supplier;
-        }
-
-        public A build() {
-            A classifier = getSupplier().get();
-            if(classifier instanceof EnhancedAbstractClassifier) {
-                ((EnhancedAbstractClassifier) classifier).setClassifierName(getName());
-            }
-            return classifier;
-        }
-
-        @Override public String toString() {
-            return name;
-        }
-    }
 
     private static ClassifierBuilderFactory<Classifier> INSTANCE;
     private final Map<String, ClassifierBuilder<? extends B>> classifierBuildersByName = new HashMap<>();
     private final Set<ClassifierBuilder<? extends B>> classifierBuilders = new HashSet<>();
+    private String name = "";
 
-    public ClassifierBuilderFactory() {}
+    public ClassifierBuilderFactory(String name) {
+        setName(name);
+    }
 
-    @Override public String toString() {
+    public ClassifierBuilderFactory() {
+        setName(getClass().getSimpleName());
+    }
+
+    public static ClassifierBuilderFactory<Classifier> getGlobalInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ClassifierBuilderFactory<>("global");
+            INSTANCE.addAll(KNNLOOCV.FACTORY);
+            INSTANCE.addAll(ElasticEnsemble.FACTORY);
+        }
+        INSTANCE.addAll(new CompileTimeClassifierBuilderFactory<KNN>());
+        return INSTANCE;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(getGlobalInstance().toString());
+    }
+
+    public List<String> getClassifierNames() {
+        return Lists.newArrayList(classifierBuildersByName.keySet());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ClassifierBuilderFactory<B> setName(final String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(getClass().getSimpleName()).append("{").append(System.lineSeparator());
+        stringBuilder.append(name).append("{").append(System.lineSeparator());
         for(Map.Entry<String, ClassifierBuilder<? extends B>> entry : classifierBuildersByName.entrySet()) {
             stringBuilder.append("\t");
             stringBuilder.append(entry.getKey());
@@ -71,16 +70,6 @@ public class ClassifierBuilderFactory<B extends Classifier> {
         }
         stringBuilder.append("}");
         return stringBuilder.toString();
-    }
-
-    public static ClassifierBuilderFactory<Classifier> getGlobalInstance() {
-        if(INSTANCE == null) {
-            INSTANCE = new ClassifierBuilderFactory<>();
-            INSTANCE.addAll(KNNLOOCV.FACTORY);
-            INSTANCE.addAll(ElasticEnsemble.FACTORY);
-        }
-        INSTANCE.addAll(new CompileTimeClassifierBuilderFactory<KNN>());
-        return INSTANCE;
     }
 
     public void addAll(ClassifierBuilderFactory<? extends B> other) {
@@ -108,7 +97,7 @@ public class ClassifierBuilderFactory<B extends Classifier> {
 
     public <C extends ClassifierBuilder<? extends B>> C add(C classifierBuilder) {
         String name = classifierBuilder.getName();
-        name = name.toLowerCase();
+        name = convertName(name);
         if(classifierBuildersByName.containsKey(name)) {
             throw new IllegalArgumentException("oops, a classifier already exists under the name: " + name);
         } else if(classifierBuilders.contains(classifierBuilder)) {
@@ -120,8 +109,12 @@ public class ClassifierBuilderFactory<B extends Classifier> {
         return classifierBuilder;
     }
 
+    private String convertName(String name) {
+        return name.toUpperCase();
+    }
+
     public ClassifierBuilder<? extends B> getClassifierBuilderByName(String name) {
-        name = name.toLowerCase();
+        name = convertName(name);
         ClassifierBuilder<? extends B> classifierBuilder = classifierBuildersByName.get(name);
         return classifierBuilder;
     }
@@ -138,7 +131,49 @@ public class ClassifierBuilderFactory<B extends Classifier> {
         return ImmutableSet.copyOf(set);
     }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(getGlobalInstance().toString());
+    public interface ClassifierBuilder<A extends Classifier> extends Supplier<A> {
+
+        String toString();
+
+        String getName();
+
+        A build();
+
+        @Override
+        default A get() {
+            return build();
+        }
+    }
+
+    public static class SuppliedClassifierBuilder<A extends Classifier> implements ClassifierBuilder<A> {
+
+        private final String name;
+        private final Supplier<? extends A> supplier;
+
+        public SuppliedClassifierBuilder(final String name, final Supplier<? extends A> supplier) {
+            this.name = name;
+            this.supplier = supplier;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Supplier<? extends A> getSupplier() {
+            return supplier;
+        }
+
+        public A build() {
+            A classifier = getSupplier().get();
+            if(classifier instanceof EnhancedAbstractClassifier) {
+                ((EnhancedAbstractClassifier) classifier).setClassifierName(getName());
+            }
+            return classifier;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
 }
