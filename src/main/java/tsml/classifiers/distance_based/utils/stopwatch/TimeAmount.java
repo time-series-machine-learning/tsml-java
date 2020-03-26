c3ed0d83455851f90b938c1348f8961aaf707af1
George Oastler <goastler4@gmail.com>
2020-03-26 13:40:04 +0000

    Represent a train contract as an amount + time unit

---
+++ src/main/java/tsml/classifiers/distance_based/utils/stopwatch/TimeAmount.java
@@ -0,0 +1,57 @@
+package tsml.classifiers.distance_based.utils.stopwatch;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Purpose: // todo - docs - type the purpose of the code here
+ * <p>
+ * Contributors: goastler
+ */
+public class TimeAmount implements Comparable<TimeAmount> {
+
+    private long amount;
+    private TimeUnit unit;
+
+    public TimeAmount() {
+        this(0, TimeUnit.NANOSECONDS);
+    }
+
+    public TimeAmount(long amount, TimeUnit unit) {
+        setAmount(amount);
+        setUnit(unit);
+    }
+
+    @Override
+    public String toString() {
+        return getAmount() + " " + getUnit();
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public TimeAmount setAmount(final long amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public TimeUnit getUnit() {
+        return unit;
+    }
+
+    public TimeAmount setUnit(final TimeUnit unit) {
+        this.unit = unit;
+        return this;
+    }
+
+    public TimeAmount convert(TimeUnit unit) {
+        return new TimeAmount(unit.convert(getAmount(), getUnit()), unit);
+    }
+
+    @Override
+    public int compareTo(final TimeAmount other) {
+        TimeAmount otherNanos = other.convert(TimeUnit.NANOSECONDS);
+        TimeAmount nanos = convert(TimeUnit.NANOSECONDS);
+        return (int) (nanos.getAmount() - otherNanos.getAmount());
+    }
+}
