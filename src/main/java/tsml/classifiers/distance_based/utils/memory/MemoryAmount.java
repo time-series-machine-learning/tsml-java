5d79f972831e8b4d64e07fb4075fa6d998db691f
George Oastler <goastler4@gmail.com>
2020-03-27 00:11:14 +0000

    memory classes

--- src/main/java/tsml/classifiers/distance_based/utils/memory/MemoryAmount.java
+++ src/main/java/tsml/classifiers/distance_based/utils/memory/MemoryAmount.java
@@ -1,5 +1,60 @@
 package tsml.classifiers.distance_based.utils.memory;
 
-public class MemoryAmount {
+/**
+ * Purpose: // todo - docs - type the purpose of the code here
+ * <p>
+ * Contributors: goastler
+ */
+public class MemoryAmount implements Comparable<MemoryAmount> {
 
+    private long amount;
+    private MemoryUnit unit;
+
+    public MemoryAmount() {
+        this(0, MemoryUnit.BYTES);
+    }
+
+    public MemoryAmount(long amount, MemoryUnit unit) {
+        setAmount(amount);
+        setUnit(unit);
+    }
+
+    public static MemoryAmount parse(String amount, String unit) {
+        unit = unit.toUpperCase();
+        return new MemoryAmount(Long.parseLong(amount), MemoryUnit.valueOf(unit));
+    }
+
+    @Override
+    public String toString() {
+        return getAmount() + " " + getUnit();
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public MemoryAmount setAmount(final long amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public MemoryUnit getUnit() {
+        return unit;
+    }
+
+    public MemoryAmount setUnit(final MemoryUnit unit) {
+        this.unit = unit;
+        return this;
+    }
+
+    public MemoryAmount convert(MemoryUnit unit) {
+        return new MemoryAmount(unit.convert(getAmount(), getUnit()), unit);
+    }
+
+    @Override
+    public int compareTo(final MemoryAmount other) {
+        MemoryAmount otherNanos = other.convert(MemoryUnit.BYTES);
+        MemoryAmount nanos = convert(MemoryUnit.BYTES);
+        return (int) (nanos.getAmount() - otherNanos.getAmount());
+    }
 }
--- src/main/java/tsml/classifiers/distance_based/utils/memory/MemoryUnit.java
+++ src/main/java/tsml/classifiers/distance_based/utils/memory/MemoryUnit.java
@@ -1,5 +1,53 @@
 package tsml.classifiers.distance_based.utils.memory;
 
-public class MemoryUnit {
+import org.junit.Assert;
+import org.junit.Test;
 
+public enum MemoryUnit {
+    BYTES,
+    KIBIBYTES,
+    MEBIBYTES,
+    GIBIBYTES,
+    ;
+
+    public static long FACTOR = 1024;
+
+    public long convert(long amount, MemoryUnit unit) {
+        int factorDifference = ordinal() - unit.ordinal();
+        if(factorDifference > 0) {
+            return amount / (FACTOR * factorDifference);
+        } else {
+            return amount * (FACTOR * -factorDifference);
+        }
+    }
+
+    public long toBytes(long amount) {
+        return convert(amount, BYTES);
+    }
+
+    public long toKibibytes(long amount) {
+        return convert(amount, KIBIBYTES);
+    }
+
+    public long toMebibytes(long amount) {
+        return convert(amount, MEBIBYTES);
+    }
+
+    public long toGibibytes(long amount) {
+        return convert(amount, GIBIBYTES);
+    }
+
+    public static class UnitTests {
+        @Test
+        public void gibibyteToMebibyte() {
+            long amount = MEBIBYTES.convert(8, GIBIBYTES);
+            Assert.assertEquals(amount, 8192);
+        }
+
+        @Test
+        public void mebibyteToGibibyte() {
+            long amount = GIBIBYTES.convert(8192, MEBIBYTES);
+            Assert.assertEquals(amount, 8);
+        }
+    }
 }
