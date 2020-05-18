00c85f38bf4a9d08f7d2d475fa4d89c1088800d6
TonyBagnall <ajb@uea.ac.uk>
2020-05-18 11:32:36 +0100

    typo

--- src/main/java/examples/Ex07_HIVE_COTE_Examples.java
+++ src/main/java/examples/Ex07_HIVE_COTE_Examples.java
@@ -18,7 +18,7 @@ import java.util.concurrent.TimeUnit;
  * HIVE-COTE version 1.0: published here
  *
  */
-public class EX07_HIVE_COTE_Examples {
+public class Ex07_HIVE_COTE_Examples {
 
     public static void basicUsage() throws Exception {
         System.out.println(" Basic Usage: load a dataset, build a default config classifier, make some predictions:");
