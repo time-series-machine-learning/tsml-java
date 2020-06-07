907bf30f41453b335a42038360606d677441e56e
George Oastler <goastler4@gmail.com>
2020-06-07 12:31:54 +0100

    Refactor PS

--- src/main/java/tsml/classifiers/distance_based/proximity/ProximitySplit.java
+++ src/main/java/tsml/classifiers/distance_based/proximity/ProximitySplit.java
@@ -9,13 +9,13 @@ import java.util.Random;
 import org.junit.Assert;
 import tsml.classifiers.distance_based.distances.DistanceMeasureable;
 import tsml.classifiers.distance_based.utils.collections.PrunedMultimap;
+import tsml.classifiers.distance_based.utils.collections.PrunedMultimap.DiscardType;
 import tsml.classifiers.distance_based.utils.params.ParamSet;
 import tsml.classifiers.distance_based.utils.params.ParamSpace;
 import tsml.classifiers.distance_based.utils.params.iteration.RandomSearchIterator;
 import tsml.classifiers.distance_based.utils.random.RandomUtils;
 import tsml.classifiers.distance_based.utils.scoring.Scorer;
 import tsml.classifiers.distance_based.utils.scoring.Scorer.GiniImpurityEntropy;
-import tsml.classifiers.distance_based.utils.strings.StrUtils;
 import utilities.ArrayUtilities;
 import utilities.Utilities;
 import weka.core.DistanceFunction;
@@ -29,39 +29,72 @@ import weka.core.Instances;
  */
 public class ProximitySplit {
 
+    // whether to early abandon distance measurements for distance between instances (data) and exemplars
     private boolean earlyAbandonDistances;
+    // the distance function for comparing instances to exemplars
     private DistanceFunction distanceFunction;
-    private List<List<Instance>> exemplars;
+    // the groups of exemplars. Each sub list is a group of exemplars. Each group becomes a branch eventually. 
+    // Instances are classified based on their proximity to the nearest group of exemplars
+    private List<List<Instance>> exemplarGroups;
+    // whether to random tie break distances (e.g. exemplar A and B have a distance of 3.5 to instance X, which to 
+    // choose?)
     private boolean randomTieBreakDistances;
+    // whether to random tie break R. R splits are considered during building this split. If multiple splits have the
+    // same score, which should be chosen?
     private boolean randomTieBreakR;
+    // the number of splits to consider for this split
     private int r;
+    // whether to choose the number of splits randomly
+    private boolean randomR; // todo random r
+    // the max number of splits to be considered. This only matters when using randomR
+    private int maxR;
+    // the random source
     private Random random;
+    // a method of scoring the split of data into partitions
     private Scorer scorer;
+    // the score of this split
     private double score;
+    // the data at this split (i.e. before being partitioned)
     private Instances data;
+    // the partitions of the data after being split
     private List<Instances> partitions;
+    // the distance function space builders. Several distance function spaces depend on the input data to compute 
+    // parameters. These builders take a set of data and compute the spaces from the data
     private List<DistanceFunctionSpaceBuilder> distanceFunctionSpaceBuilders;
+    // the distance function space builder chosen for this split
     private DistanceFunctionSpaceBuilder distanceFunctionSpaceBuilder;
+    // the distance function space chosen for this split
     private ParamSpace distanceFunctionSpace;
+    // whether to match the original Proximity Forest results exactly. This is only useful if mirroring PF parameters
+    // exactly
+    private boolean matchOriginalPFRandomCalls;
+    // whether to check for exemplar matching inside the loop (original) or before any looping (improved method)
+    private boolean exemplarCheckOriginal;
 
+    /**
+     * 
+     * @param random the random source
+     */
     public ProximitySplit(Random random) {
         setRandom(random);
+        // init to defaults
         setR(5);
         setRandomTieBreakDistances(true);
         setRandomTieBreakR(false);
         setEarlyAbandonDistances(false);
         setScorer(new GiniImpurityEntropy());
         setScore(-1);
+        setRandomR(false);
+        setExemplarCheckOriginal(true); // todo turn on/off in PT and PF
+        setMatchOriginalPFRandomCalls(false); // todo turn on in PT and PF
     }
 
-    private void pickExemplars(final Instances instances) {
-//        System.out.println("pe"); // todo
-//        System.out.println("is: " + instances.size());
-        final Map<Double, Instances> instancesByClass = Utilities.instancesByClass(instances);
-        for(Double classLabel : instancesByClass.keySet()) {
-            final Instances instanceClass = instancesByClass.get(classLabel);
-//            System.out.println("cc: " + instanceClass.size());
-        }
+    /**
+     * pick exemplars from the given dataset
+     * @param instancesByClass a map of class labels to instances
+     */
+    private void pickExemplars(final Map<Double, Instances> instancesByClass) {
+        // pick one exemplar per class
         List<List<Instance>> exemplars = Lists.newArrayList(instancesByClass.size());
         for(Double classLabel : instancesByClass.keySet()) {
             final Instances instanceClass = instancesByClass.get(classLabel);
@@ -74,29 +107,41 @@ public class ProximitySplit {
             }
             exemplars.add(Lists.newArrayList(exemplar));
         }
-        setExemplars(exemplars);
+        setExemplarGroups(exemplars);
     }
 
+    /**
+     * pick the distance function
+     */
     private void pickDistanceFunction() {
-//        System.out.println("pd"); // todo
+        // pick a random space
         distanceFunctionSpaceBuilder = RandomUtils.choice(distanceFunctionSpaceBuilders, getRandom());
+        // built that space
         distanceFunctionSpace = distanceFunctionSpaceBuilder.build(data);
+        // randomly pick the distance function / parameters from that space
         RandomSearchIterator iterator = new RandomSearchIterator(getRandom(), distanceFunctionSpace);
         ParamSet paramSet = iterator.next();
+        // there is only one distance function in the ParamSet returned
         List<Object> list = paramSet.get(DistanceMeasureable.getDistanceFunctionFlag());
         Assert.assertEquals(1, list.size());
+        // cast it into shape
         Object obj = list.get(0);
         setDistanceFunction((DistanceFunction) obj);
     }
 
+    /**
+     * build the split using the data provided
+     */
     public void buildSplit() {
-        class Container {
+        // small helper class to contain a split. This is used to temporarily hold split results while this function 
+        // compares R splits to pick the best
+        class SplitCandididate {
             public final List<List<Instance>> exemplars;
             public final DistanceFunction distanceFunction;
             public final List<Instances> partitions;
             public final double score;
 
-            Container(final List<List<Instance>> exemplars, final DistanceFunction distanceFunction,
+            SplitCandididate(final List<List<Instance>> exemplars, final DistanceFunction distanceFunction,
                 final List<Instances> partitions, final double score) {
                 this.exemplars = exemplars;
                 this.distanceFunction = distanceFunction;
@@ -104,51 +149,64 @@ public class ProximitySplit {
                 this.score = score;
             }
         }
-        PrunedMultimap<Double, Container> map = PrunedMultimap.desc();
+        // change the view of the data into per class
+        final Map<Double, Instances> instancesByClass = Utilities.instancesByClass(data);
+        // make a map to store the best X splits
+        final PrunedMultimap<Double, SplitCandididate> map = PrunedMultimap.desc();
         if(randomTieBreakR) {
+            // splits which score the same will be kept, then random choice between them
             map.setSoftLimit(1);
         } else {
+            // splits which score the same will not be kept, only 1 will be kept at any point in time
             map.setHardLimit(1);
+            // discard the youngest split. i.e. if there's 2 splits which score 0.4, the newest split will be discarded
+            map.setDiscardType(DiscardType.YOUNGEST);
         }
+        // randomly set R if enabled
+        if(randomR) {
+            maxR = r;
+            r = random.nextInt(maxR + 1) + 1;
+        }
+        // for every split attempt
         for(int i = 0; i < r; i++) {
+            // pick the distance function
             pickDistanceFunction();
-            pickExemplars(data); // todo can reuse class counts
-            List<Instances> partitions = Lists.newArrayList(exemplars.size());
+            // pick the exemplars
+            pickExemplars(instancesByClass);
+            // setup the partitions
+            List<Instances> partitions = Lists.newArrayList(exemplarGroups.size());
             if(distanceFunction instanceof DistanceMeasureable) {
                 ((DistanceMeasureable) distanceFunction).setTraining(true);
             }
-            for(List<Instance> group : exemplars) {
+            for(List<Instance> group : exemplarGroups) {
                 partitions.add(new Instances(data, 0));
             }
             distanceFunction.setInstances(data);
+            // go through every instance and find which partition it should go into. This should be the partition 
+            // with the closest exemplar associate
             for(int j = 0; j < data.size(); j++) {
                 final Instance instance = data.get(j);
                 final int index = getPartitionIndexFor(instance);
-//                System.out.println("cb: " + j + "," + index); // todo
                 final Instances closestPartition = partitions.get(index);
                 closestPartition.add(instance);
             }
             if(distanceFunction instanceof DistanceMeasureable) {
                 ((DistanceMeasureable) distanceFunction).setTraining(false);
             }
+            // find the score of this split attempt, i.e. how good it is
             double score = scorer.findScore(data, partitions);
-            Container container = new Container(exemplars, distanceFunction, partitions, score);
-            map.put(score, container);
-//            System.out.println("g: " + Utilities.roundExact(0.5 - score, 8)); // todo
+            // chuck into a container to keep for later
+            SplitCandididate splitCandididate = new SplitCandididate(exemplarGroups, distanceFunction, partitions, score);
+            // add it to the map. The map will handle whether the split attempt was any good and should be kept
+            map.put(score, splitCandididate);
         }
-        Container choice = RandomUtils.choice(new ArrayList<>(map.values()), random);
+        // choose the best of the R splits. The map handles the tie break if necessary
+        SplitCandididate choice = RandomUtils.choice(new ArrayList<>(map.values()), random);
+        // populate the fields of this split from the split attempt
         partitions = choice.partitions;
         distanceFunction = choice.distanceFunction;
-        exemplars = choice.exemplars;
+        exemplarGroups = choice.exemplars;
         score = choice.score;
-//        System.out.println(distanceFunction.getClass() + " " + StrUtils.join(" ", distanceFunction.getOptions()));
-//        System.out.println("bg: " + Utilities.roundExact(0.5 - score, 8)); // todo
-//        if(0.5 - score == 0.26666666666666666) {
-//            System.out.println("stop here");
-//        }
-        for(Instances part : partitions) {
-//            System.out.println("part: " + part.size());
-        }
     }
 
     public Instances getPartitionFor(Instance instance) {
@@ -228,50 +286,89 @@ public class ProximitySplit {
         return score;
     }
 
+    /**
+     * get the partition index of the given instance
+     * @param instance
+     * @return
+     */
     public int getPartitionIndexFor(final Instance instance) {
+        // check the instance isn't an exemplar. If it is, it must belong to the associated partition. No need for
+        // distance measurements at all
+        if(!exemplarCheckOriginal) {
+            // for each exemplar group
+            for(int i = 0; i < exemplarGroups.size(); i++) {
+                final List<Instance> group = exemplarGroups.get(i);
+                // for each exemplar
+                for(Instance exemplar : group) {
+                    if(exemplar.equals(instance)) {
+                        return i;
+                    }
+                }
+            }
+        }
+        // the maximum possible distance returned by the distance function
         final double maxDistance = Double.POSITIVE_INFINITY;
+        // the limit for early abandon
         double limit = maxDistance;
+        // a map to maintain the closest partition indices
         PrunedMultimap<Double, Integer> distanceToPartitionIndexMap = PrunedMultimap.asc();
         if(randomTieBreakDistances) {
+            // let the map keep all ties and randomly choose at the end
             distanceToPartitionIndexMap.setSoftLimit(1);
         } else {
+            // only keep 1 partition at any point in time, even if multiple partitions are equally close
             distanceToPartitionIndexMap.setHardLimit(1);
+            // discard the newest on tie break situation
+            distanceToPartitionIndexMap.setDiscardType(DiscardType.YOUNGEST);
         }
-        double minDistance = maxDistance;
-        for(int i = 0; i < exemplars.size(); i++) {
-            for(Instance exemplar : exemplars.get(i)) {
-                if(exemplar.equals(instance)) {
-                    return i; // move this check to outside of the loop (i.e. if we've received the second exemplar
-                    // we end up doing the distance to the first which is pointless
+        // loop through exemplar groups
+        for(int i = 0; i < exemplarGroups.size(); i++) {
+            // for each exemplar in the current group
+            for(Instance exemplar : exemplarGroups.get(i)) {
+                // check the instance isn't an exemplar
+                if(exemplarCheckOriginal && exemplar.equals(instance)) {
+                    return i;
                 }
+                // find the distance
                 final double distance = distanceFunction.distance(instance, exemplar, limit);
-//                System.out.println(Utilities.roundExact(distance, 8));
+                // adjust early abandon
                 if(earlyAbandonDistances) {
                     limit = Math.min(distance, limit);
                 }
-                minDistance = Math.min(distance, minDistance);
+                // add the distance and partition index to the map
                 distanceToPartitionIndexMap.put(distance, i);
             }
         }
+        // get the smallest distance from the map
         final Double smallestDistance = distanceToPartitionIndexMap.firstKey();
-        final Collection<Integer> closestPartitionIndices = distanceToPartitionIndexMap.get(smallestDistance);
+        // find the list of corresponding partition indices
+        final List<Integer> closestPartitionIndices = distanceToPartitionIndexMap.get(smallestDistance);
+        final int numIndices = closestPartitionIndices.size();
         if(!randomTieBreakDistances) {
-            Assert.assertEquals(closestPartitionIndices.size(), 1);
+            Assert.assertEquals(numIndices, 1);
         }
+        // random pick the best index
+        final Integer closestPartitionIndex = Utilities.randPickOne(closestPartitionIndices, getRandom());
         // no-op, but must sample the random source to match orig pf. The orig implementation always samples the
         // random irrelevant of whether the list size is 1 or more. We only sample IFF the list size is larger than 1
         // . Therefore we'll sample the random if the list is exactly equal to 1 in size.
-        if(closestPartitionIndices.size() == 1) {
-            getRandom().nextInt(closestPartitionIndices.size());
+        if(matchOriginalPFRandomCalls && numIndices == 1) {
+            getRandom().nextInt(numIndices);
         }
-        final Integer closestPartitionIndex = Utilities.randPickOne(closestPartitionIndices, getRandom());
         return closestPartitionIndex;
     }
 
+    /**
+     * find the distribution for the given instance and partition index it belongs to
+     * @param instance the instance
+     * @param index the index of the partition it belongs to
+     * @return the distribution
+     */
     public double[] distributionForInstance(final Instance instance, int index) {
+        // this is a simple majority vote over all the exemplars in the exemplars group at the given partition
         // get the corresponding closest exemplars
-        List<Instance> exemplars = this.exemplars.get(index);
-        double[] distribution = new double[instance.numClasses()];
+        final List<Instance> exemplars = exemplarGroups.get(index);
+        final double[] distribution = new double[instance.numClasses()];
         // for each exemplar
         for(Instance exemplar : exemplars) {
             // vote for the exemplar's class
@@ -294,17 +391,17 @@ public class ProximitySplit {
         return distanceFunction;
     }
 
-    public List<List<Instance>> getExemplars() {
-        return exemplars;
+    public List<List<Instance>> getExemplarGroups() {
+        return exemplarGroups;
     }
 
-    private void setExemplars(final List<List<Instance>> exemplars) {
-        Assert.assertNotNull(exemplars);
-        for(List<Instance> exemplarGroup : exemplars) {
+    private void setExemplarGroups(final List<List<Instance>> exemplarGroups) {
+        Assert.assertNotNull(exemplarGroups);
+        for(List<Instance> exemplarGroup : exemplarGroups) {
             Assert.assertNotNull(exemplarGroup);
             Assert.assertFalse(exemplarGroup.isEmpty());
         }
-        this.exemplars = exemplars;
+        this.exemplarGroups = exemplarGroups;
     }
 
     private void setDistanceFunction(final DistanceFunction distanceFunction) {
@@ -354,5 +451,29 @@ public class ProximitySplit {
 
     public ParamSpace getDistanceFunctionSpace() {
         return distanceFunctionSpace;
+    }
+
+    public boolean isRandomR() {
+        return randomR;
+    }
+
+    public void setRandomR(final boolean randomR) {
+        this.randomR = randomR;
+    }
+
+    public int getMaxR() {
+        return maxR;
+    }
+
+    public boolean isMatchOriginalPFRandomCalls() {
+        return matchOriginalPFRandomCalls;
+    }
+
+    public void setMatchOriginalPFRandomCalls(final boolean matchOriginalPFRandomCalls) {
+        this.matchOriginalPFRandomCalls = matchOriginalPFRandomCalls;
+    }
+
+    public void setExemplarCheckOriginal(final boolean exemplarCheckOriginal) {
+        this.exemplarCheckOriginal = exemplarCheckOriginal;
     }
 }
