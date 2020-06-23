47d9fe32027e00f179bc8562714f3d638bae7ab1
George Oastler <goastler4@gmail.com>
2020-06-23 11:28:36 +0100

    fixed / refactored intervals

---
+++ src/main/java/tsml/classifiers/distance_based/utils/intervals/Interval.java
@@ -0,0 +1,90 @@
+package tsml.classifiers.distance_based.utils.intervals;
+
+import tsml.classifiers.distance_based.utils.params.ParamHandler;
+import tsml.classifiers.distance_based.utils.params.ParamHandlerUtils;
+import tsml.classifiers.distance_based.utils.params.ParamSet;
+import utilities.Utilities;
+
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Purpose: represent an interval, i.e. some subsequence of indices. An interval therefore has a start and end point
+ * (inclusively!). The start point may be beyond the end point to reverse directionality.
+ *
+ * Contributors: goastler
+ */
+public class Interval implements ParamHandler {
+    private int start;
+    private int end;
+    public static final String START_FLAG = "s";
+    public static final String END_FLAG = "e";
+
+    public Interval() {
+        this(-1, -1);
+    }
+
+    public Interval(final int start, final int end) {
+        setEnd(end);
+        setStart(start);
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(final int end) {
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(final int start) {
+        this.start = start;
+    }
+
+    public int size() {
+        return Math.abs(end - start) + 1;
+    }
+
+    /**
+     * map interval index to instance index
+     * @param index
+     * @return
+     */
+    public int translate(int index) {
+        index = adjustIndex(index);
+        index += Math.min(start, end);
+        return index;
+    }
+
+    public int adjustIndex(int index) {
+        if(start > end) {
+            index = size() - index - 1;
+        }
+        return index;
+    }
+
+    /**
+     * map instance index to interval index
+     * @param index
+     * @return
+     */
+    public int inverseTranslate(int index) {
+        index -= Math.min(start, end);
+        index = adjustIndex(index);
+        return index;
+    }
+
+    @Override public ParamSet getParams() {
+        return ParamHandler.super.getParams().add(START_FLAG, start).add(END_FLAG, end);
+    }
+
+    @Override public void setParams(final ParamSet paramSet) throws Exception {
+        ParamHandler.super.setParams(paramSet);
+        ParamHandlerUtils.setParam(paramSet, START_FLAG, this::setStart, Integer.class);
+        ParamHandlerUtils.setParam(paramSet, END_FLAG, this::setEnd, Integer.class);
+    }
+}
---
+++ src/main/java/tsml/classifiers/distance_based/utils/intervals/IntervalTest.java
@@ -0,0 +1,68 @@
+package tsml.classifiers.distance_based.utils.intervals;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntervalTest {
+
+    private Interval interval;
+
+    @Before
+    public void before() {
+        interval = new Interval();
+        interval.setStart(50);
+        interval.setEnd(60);
+    }
+
+    private void reverseInterval() {
+        final int end = interval.getEnd();
+        interval.setEnd(interval.getStart());
+        interval.setStart(end);
+    }
+
+    @Test
+    public void testIntervalSize() {
+        Assert.assertEquals(11, interval.size());
+    }
+
+    @Test
+    public void testReversedIntervalSize() {
+        reverseInterval();
+        Assert.assertEquals(11, interval.size());
+    }
+
+    @Test
+    public void testIntervalIndexToAttributeIndex() {
+        for(int i = 0; i < 11; i++) {
+            final int index = interval.translate(i);
+            Assert.assertEquals(i + interval.getStart(), index);
+        }
+    }
+
+    @Test
+    public void testAttributeIndexToIntervalIndex() {
+        for(int i = 0; i < 11; i++) {
+            final int index = interval.inverseTranslate(i + interval.getStart());
+            Assert.assertEquals(i, index);
+        }
+    }
+
+    @Test
+    public void testReversedIntervalIndexToAttributeIndex() {
+        reverseInterval();
+        for(int i = 0; i < 11; i++) {
+            final int index = interval.translate(i);
+            Assert.assertEquals(interval.getStart() - i, index);
+        }
+    }
+
+    @Test
+    public void testReversedAttributeIndexToIntervalIndex() {
+        reverseInterval();
+        for(int i = 0; i < 11; i++) {
+            final int index = interval.inverseTranslate(interval.getStart() - i);
+            Assert.assertEquals(i, index);
+        }
+    }
+}
