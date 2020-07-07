b388585f9d760c85b621d30f3d28525b5f8cafd6
vrq16kku <vrq16kku@uea.ac.uk>
2020-07-07 16:57:03 +0100

    Implemented basic ShapeDTW

--- src/main/java/tsml/contrib/ShapeDTW1NN.java
+++ src/main/java/tsml/contrib/ShapeDTW1NN.java
@@ -1,4 +1,121 @@
 package tsml.contrib;
 
-public class ShapeDTW1NN {
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import tsml.classifiers.EnhancedAbstractClassifier;
+import tsml.contrib.transformers.SubsequenceTransformer;
+import tsml.transformers.Derivative;
+import tsml.transformers.PAA;
+import tsml.transformers.Transformer;
+import weka.core.Instance;
+import weka.core.Instances;
+import java.util.concurrent.TimeUnit;
+import tsml.classifiers.multivariate.NN_DTW_D;
+
+
+public class ShapeDTW1NN extends EnhancedAbstractClassifier {
+
+    // hyper-parameters
+    private int subsequenceLength;
+    // if shapeDescriptors has length>1, then its a 'compound' shape descriptor.
+    // if null, then its the 'raw' shape descriptor.
+    // Supported Transformers are the following:
+    // null - raw
+    // PAA
+    // DWT
+    // Derivative
+    // Slope
+    // HOG1D
+    // 2 of these transformers
+    private Transformer [] shapeDescriptors;
+    // Transformer for extracting the neighbourhoods
+    private SubsequenceTransformer subsequenceTransformer;
+    // NN_DTW_D for performing classification on the training data
+    private NN_DTW_D nnDtwD;
+    // internal attributes
+    private int numClasses;
+
+    /**
+     * Private constructor so that ShapeDTW can be used out-of-the-box
+     */
+    public ShapeDTW1NN() {
+        super(CANNOT_ESTIMATE_OWN_PERFORMANCE);
+        this.subsequenceLength = 30;
+        this.shapeDescriptors = null;
+        this.subsequenceTransformer = new SubsequenceTransformer(subsequenceLength);
+        this.nnDtwD = new NN_DTW_D();
+    }
+
+    public ShapeDTW1NN(int subsequenceLength,Transformer [] shapeDescriptors) {
+        super(CANNOT_ESTIMATE_OWN_PERFORMANCE);
+        this.subsequenceLength = subsequenceLength;
+        this.shapeDescriptors = shapeDescriptors;
+        this.subsequenceTransformer = new SubsequenceTransformer(subsequenceLength);
+        this.nnDtwD = new NN_DTW_D();
+    }
+
+    public int getSubsequenceLength() {
+        return subsequenceLength;
+    }
+
+    public Transformer [] getShapeDescriptors() {
+        return shapeDescriptors;
+    }
+
+    public void setSubsequenceLength(int subsequenceLength) {
+        this.subsequenceLength = subsequenceLength;
+    }
+
+    public void setShapeDescriptors(Transformer [] shapeDescriptors) {
+        this.shapeDescriptors = shapeDescriptors;
+    }
+
+    private Instances preprocessData(Instances data) {
+        Instances transformedData = this.subsequenceTransformer.transform(data);
+        if (this.shapeDescriptors == null) {
+            return transformedData;
+        }
+        for(int i=0;i<this.shapeDescriptors.length;i++) {
+            transformedData = this.shapeDescriptors[i].transform(transformedData);
+        }
+        return transformedData;
+    }
+
+    private Instance preprocessData(Instance data) {
+        Instance transformedData = this.subsequenceTransformer.transform(data);
+        if (this.shapeDescriptors == null) {
+            return transformedData;
+        }
+        for(int i=0;i<this.shapeDescriptors.length;i++) {
+            transformedData = this.shapeDescriptors[i].transform(transformedData);
+        }
+        return transformedData;
+    }
+
+    @Override
+    public void buildClassifier(Instances trainInst) throws Exception {
+        // Check the data
+        this.getCapabilities().testWithFail(trainInst);
+        // Record the build time.
+        long buildTime = System.nanoTime();
+        // Train the classifier
+        Instances transformedData = this.preprocessData(trainInst);
+        this.nnDtwD.buildClassifier(transformedData);
+        // Store the timing results.
+        buildTime = System.nanoTime() - buildTime ;
+        this.trainResults.setBuildTime(buildTime);
+        this.trainResults.setTimeUnit(TimeUnit.NANOSECONDS);
+    }
+
+    @Override
+    public double [] distributionForInstance(Instance testInst) throws Exception {
+        Instance transformedData = this.preprocessData(testInst);
+        return this.nnDtwD.distributionForInstance(transformedData);
+    }
+
+    @Override
+    public double classifyInstance(Instance testInst) throws Exception {
+        Instance transformedData = this.preprocessData(testInst);
+        return this.nnDtwD.classifyInstance(transformedData);
+    }
+
 }
--- src/main/java/tsml/contrib/transformers/SubsequenceTransformer.java
+++ src/main/java/tsml/contrib/transformers/SubsequenceTransformer.java
@@ -1,4 +1,134 @@
 package tsml.contrib.transformers;
 
-public class SubsequenceTransformer {
+import com.sun.org.apache.xpath.internal.operations.Mult;
+import tsml.transformers.Transformer;
+import utilities.multivariate_tools.MultivariateInstanceTools;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+public class SubsequenceTransformer implements Transformer {
+
+    private int subsequenceLength;
+    private Instances relationalHeader;
+    public SubsequenceTransformer(int subsequenceLength) {
+        this.subsequenceLength = subsequenceLength;
+    }
+
+
+    @Override
+    public Instance transform(Instance inst) {
+        double [] timeSeries = inst.toDoubleArray();
+        // remove class label
+        double[] temp;
+        int c = inst.classIndex();
+        if(!inst.classIsMissing()) {
+            temp=new double[timeSeries.length-1];
+            System.arraycopy(timeSeries,0,temp,0,c); //assumes class attribute is in last index
+            timeSeries=temp;
+        }
+        // Extract the subsequences.
+        double [] [] subsequences = extractSubsequences(timeSeries);
+        //Create the instance - contains only 2 attributes, the relation and the class value.
+        Instance newInstance = new DenseInstance(2);
+        // Create the relation object.
+        Instances relation = createRelation(timeSeries.length);
+        // Add the subsequences to the relation object
+        for(double [] list : subsequences) {
+            Instance newSubsequence = new DenseInstance(1.0,list);
+            relation.add(newSubsequence);
+        }
+        // set the dataset for the newInstance
+        newInstance.setDataset(this.relationalHeader);
+        // Add the relation to the first attribute.
+        int index = newInstance.attribute(0).addRelation(relation);
+        newInstance.setValue(0,index);
+        // Add the class value
+        newInstance.setValue(1,inst.classValue());
+        return newInstance;
+    }
+
+    /**
+     * Method to extract all subsequences given a time series.
+     *
+     * @param timeSeries - the time series to transform.
+     * @return a 2D array of subsequences.
+     */
+    private double [] [] extractSubsequences(double [] timeSeries) {
+        int padAmount = (int) Math.floor(subsequenceLength/2);
+        double [] paddedTimeSeries = padTimeSeries(timeSeries,padAmount);
+        double [] [] subsequences = new double [timeSeries.length] [subsequenceLength];
+        for(int i=0;i<timeSeries.length;i++) {
+            subsequences[i] = Arrays.copyOfRange(paddedTimeSeries,i,i+subsequenceLength);
+        }
+        return subsequences;
+    }
+
+    /**
+     * Private method for padding the time series on either end by padAmount times.
+     * The beginning is padded by the first value of the time series and the end
+     * is padded by the last element in the time series.
+     *
+     * @param timeSeries
+     * @param padAmount
+     * @return
+     */
+    private double [] padTimeSeries(double [] timeSeries,int padAmount) {
+        double [] newTimeSeries = new double [timeSeries.length + padAmount*2];
+        double valueToAdd;
+        for(int i=0;i<newTimeSeries.length;i++) {
+            // Add the first element
+            if(i<padAmount) {
+                valueToAdd = timeSeries[0];
+            } else if (i>(timeSeries.length)) {
+                // Add the last element
+                valueToAdd = timeSeries[timeSeries.length-1];
+            } else {
+                valueToAdd = timeSeries[i-padAmount];
+            }
+            newTimeSeries[i] = valueToAdd;
+        }
+        return newTimeSeries;
+    }
+
+    @Override
+    public Instances determineOutputFormat(Instances inputFormat) throws IllegalArgumentException {
+        if (inputFormat.classIndex() != inputFormat.numAttributes() - 1) {
+            throw new IllegalArgumentException("cannot handle class values not at end");
+        }
+        //Set up instances size and format.
+        //Create the relation.
+        Instances relation = createRelation(inputFormat.numAttributes());
+        //Create an arraylist of 2 containing the relational attribute and the class label.
+        ArrayList<Attribute> newAtts = new ArrayList<>();
+        newAtts.add(new Attribute("relationalAtt", relation));
+        newAtts.add(inputFormat.classAttribute());
+        //Put them into a new Instances object.
+        Instances newFormat = new Instances("Subsequences", newAtts, inputFormat.numInstances());
+        newFormat.setRelationName("Subsequences_" + inputFormat.relationName());
+        newFormat.setClassIndex(newFormat.numAttributes() - 1);
+        this.relationalHeader = newFormat;
+        return newFormat;
+    }
+
+    /**
+     * Private method for creating the relation as it always has the same dimensions -
+     * numAtts() = subsequenceLength and numInsts() = timeSeriesLength.
+     * @return
+     */
+    private Instances createRelation(int timeSeriesLength) {
+        ArrayList<Attribute> attributes = new ArrayList<>();
+        // Add the original elements
+        for (int i = 0; i < this.subsequenceLength; i++)
+            attributes.add(new Attribute("Subsequence_element_" + i));
+        // Number of instances is equal to the length of the time series (as that's the number of
+        // subsequences you extract)
+        Instances relation = new Instances("Subsequences", attributes, timeSeriesLength);
+        return relation;
+    }
 }
