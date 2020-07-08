00445dbedd2c31d8aa56ec2167f0d661dd6c63eb
vrq16kku <vrq16kku@uea.ac.uk>
2020-07-08 15:27:12 +0100

    started dwt

--- src/main/java/tsml/contrib/transformers/DWTTransformer.java
+++ src/main/java/tsml/contrib/transformers/DWTTransformer.java
@@ -1,4 +1,22 @@
 package tsml.contrib.transformers;
 
-public class DWTTransformer {
+import tsml.transformers.Transformer;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class DWTTransformer implements Transformer {
+
+    @Override
+    public Instance transform(Instance inst) {
+        return null;
+    }
+
+    @Override
+    public Instances determineOutputFormat(Instances data) throws IllegalArgumentException {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
