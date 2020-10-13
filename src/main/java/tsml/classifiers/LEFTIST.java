f78cb042c06b56570e1e10da03d20ccca49f8052
MatthewMiddlehurst <pfm15hbu@uea.ac.uk>
2020-10-13 10:31:47 +0100

    leftist

--- src/main/java/tsml/classifiers/LEFTIST.java
+++ src/main/java/tsml/classifiers/LEFTIST.java
@@ -2,15 +2,12 @@ package tsml.classifiers;
 
 import experiments.data.DatasetLoading;
 import fileIO.OutFile;
-import tsml.classifiers.EnhancedAbstractClassifier;
-import tsml.classifiers.dictionary_based.MultivariateIndividualTDE;
-import tsml.classifiers.dictionary_based.TDE;
 import tsml.classifiers.interval_based.CIF;
 import tsml.data_containers.TimeSeriesInstance;
 import tsml.data_containers.TimeSeriesInstances;
 import tsml.data_containers.utilities.Converter;
+import utilities.generic_storage.Pair;
 import weka.classifiers.functions.LinearRegression;
-import weka.classifiers.functions.Logistic;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
@@ -22,10 +19,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.function.Function;
 
 import static utilities.InstanceTools.resampleTrainAndTestInstances;
+import static utilities.StatisticalUtilities.dot;
 import static utilities.Utilities.argMax;
-import static weka.core.Utils.sum;
 
 public class LEFTIST {
 
@@ -34,15 +32,21 @@ public class LEFTIST {
 
     private int noSlices = 20;
     private int noNeighbours = 1000;
+
+    private boolean distanceToSeries = false;
+    private boolean predicictionClassOnly = false;
+
+    private Function<Pair<double[],double[]>,Double> similarityMeasure = (Pair<double[],double[]> p) ->
+            -(dot(p.var1,p.var2) / (Math.sqrt(dot(p.var1,p.var1)) * Math.sqrt(dot(p.var2,p.var2)))) + 1;
+    private Function<Double,Double> kernel = (Double d) ->
+            Math.sqrt(Math.exp(-Math.pow(d, 2) / Math.pow(0.25, 2)));
+
     private LinearRegression explainer = new LinearRegression();
 
     private int seed;
     private Random rand;
 
-    private boolean debug = true;
-
-    int predVal;
-    int[][] predSlices;
+    private boolean debug = false;
 
     public LEFTIST(TimeSeriesInstances train, EnhancedAbstractClassifier classifier, int seed){
         this.train = train;
@@ -66,17 +70,24 @@ public class LEFTIST {
         noNeighbours = i;
     }
 
-    public double[][] sliceWeights(TimeSeriesInstance inst) throws Exception {
+    public void setDistanceToSeries(boolean b) { distanceToSeries = b; }
+
+    public void setDistanceMeasure(Function<Pair<double[],double[]>,Double> f) { similarityMeasure = f; }
+
+    public void setKernel(Function<Double,Double> f) { kernel = f; }
+
+    public Explanation generateExplanation(TimeSeriesInstance inst) throws Exception {
         if (inst.getNumDimensions() > 1){
             System.err.println("Only available for univariate series.");
             return null;
         }
 
-        int[][] slices = slices(inst.getMaxLength());
-        predSlices = slices;
+        Explanation e = new Explanation();
+
+        e.slices = slices(inst.getMaxLength());
         double[][] activatedSlices = neighbourSliceActivation();
-        double[][] transformedNeighbours = transformNeighbours(inst, slices, activatedSlices);
-        double[] neighbourWeights = weightNeighbours(activatedSlices); //neighbours or masks?
+        double[][] transformedNeighbours = transformNeighbours(inst, e.slices, activatedSlices);
+        double[] neighbourWeights = weightNeighbours(distanceToSeries ? transformedNeighbours : activatedSlices);
 
         double[][] probas = new double[noNeighbours][];
         for (int i = 0; i < noNeighbours; i++) {
@@ -84,12 +95,22 @@ public class LEFTIST {
             dims[0] = transformedNeighbours[i];
             probas[i] = classifier.distributionForInstance(new TimeSeriesInstance(dims));
         }
-        predVal = argMax(probas[0], rand);
+        e.predVal = argMax(probas[0], rand);
 
         //feature selection?
 
-        int noClasses = train.getClassLabels().length;
-        double[][] coeffs = new double[noClasses][];
+        int noClasses;
+        if (predicictionClassOnly){
+            noClasses = 1;
+            e.classes = new int[]{ e.predVal };
+        }
+        else{
+            noClasses = train.getClassLabels().length;
+            e.classes = train.getClassIndexes();
+        }
+
+        e.coefficientsForClass = new double[noClasses][];
+        e.classMeans = new double[noClasses];
 
         ArrayList<Attribute> atts = new ArrayList<>(noSlices + 1);
         ArrayList<String> vals = new ArrayList<>(2);
@@ -109,7 +130,7 @@ public class LEFTIST {
 
         for (int i = 0 ; i < noClasses; i++) {
             for (int n = 0; n < noNeighbours; n++){
-                maskInstances.get(n).setClassValue(probas[n][i]);
+                maskInstances.get(n).setClassValue(probas[n][e.classes[i]]);
             }
 
             explainer.buildClassifier(maskInstances);
@@ -117,35 +138,36 @@ public class LEFTIST {
             //score?
 
             double[] c = explainer.coefficients();
-            coeffs[i] = new double[noSlices];
-            System.arraycopy(c, 0, coeffs[i], 0, noSlices);
+            e.coefficientsForClass[i] = new double[noSlices];
+            System.arraycopy(c, 0, e.coefficientsForClass[i], 0, noSlices);
+            e.classMeans[i] = c[c.length-1];
         }
 
-        return coeffs;
+        return e;
     }
 
-    public double[][] sliceWeights(Instance inst) throws Exception {
-        return sliceWeights(Converter.fromArff(inst));
+    public Explanation generateExplanation(Instance inst) throws Exception {
+        return generateExplanation(Converter.fromArff(inst));
     }
 
     public void outputFigure(TimeSeriesInstance inst, String figureSavePath) throws Exception {
-        double[][] weights = sliceWeights(inst);
+        Explanation exp = generateExplanation(inst);
 
         File f = new File(figureSavePath);
         if(!f.isDirectory()) f.mkdirs();
 
         OutFile of = new OutFile(figureSavePath + "\\interp" + seed + ".txt");
         of.writeLine(Arrays.toString(inst.toValueArray()[0]));
-        of.writeString(Arrays.toString(predSlices[0]));
-        for (int i = 1; i < predSlices.length; i++) {
-            of.writeString(";" + Arrays.toString(predSlices[i]));
+        of.writeString(Arrays.toString(exp.slices[0]));
+        for (int i = 1; i < exp.slices.length; i++) {
+            of.writeString(";" + Arrays.toString(exp.slices[i]));
         }
         of.writeLine("");;
-        of.writeLine(Integer.toString(predVal));
+        of.writeLine(Integer.toString(exp.predVal));
         of.writeLine(Integer.toString(train.getClassLabels().length));
-        for (int i = 0; i < train.getClassLabels().length; i++){
-            of.writeLine(Integer.toString(i));
-            of.writeLine(Arrays.toString(weights[i]));
+        for (int i = 0; i < exp.coefficientsForClass.length; i++){
+            of.writeLine(Integer.toString(exp.classes[i]));
+            of.writeLine(Arrays.toString(exp.coefficientsForClass[i]));
         }
 
         Process p = Runtime.getRuntime().exec("python src/main/python/leftist.py \"" +
@@ -220,33 +242,29 @@ public class LEFTIST {
 
     private double[] weightNeighbours(double[][] neighbours){
         double[] weights = new double[neighbours.length];
+        Pair<double[],double[]> p = new Pair<>(neighbours[0], null);
         for (int i = 0; i < noNeighbours; i++){
-            weights[i] = kernel(cosineDistance(neighbours[0], neighbours[i]));
+            p.var2 = neighbours[i];
+            weights[i] = kernel.apply(similarityMeasure.apply(p));
         }
         return weights;
     }
 
-    private double kernel(double distance){
-        return Math.sqrt(Math.exp(-Math.pow(distance, 2) / Math.pow(0.25, 2))); //kernel width of 0.25 ^2
-    }
+    public static class Explanation {
+        int predVal;
+        int[][] slices;
+        int[] classes;
+        double[][] coefficientsForClass;
+        double[] classMeans;
+        double score = -1;
 
-    private double cosineDistance(double[] inst1, double[] inst2){
-        return -(dot(inst1,inst2) / (Math.sqrt(dot(inst1,inst1)) * Math.sqrt(dot(inst2,inst2)))) + 1;
-    }
+        public Explanation() {}
 
-    private double dot(double[] inst1, double[] inst2){
-        double sum = 0;
-        for (int i = 0; i < inst1.length; i++)
-            sum += inst1[i] * inst2[i];
-        return sum;
-    }
-
-    private double euclideanDistance(double[] inst1, double[] inst2){
-        double distance = 0;
-        for (int i = 0; i < inst1.length; i++){
-            distance += Math.pow(inst1[i] - inst2[i], 2);
+        @Override
+        public String toString(){
+            return predVal + "\n" + Arrays.deepToString(slices) + "\n" + Arrays.toString(classes) + "\n" +
+                    Arrays.deepToString(coefficientsForClass) + "\n" + Arrays.toString(classMeans) + "\n" + score;
         }
-        return Math.sqrt(distance);
     }
 
     public static void main(String[] args) throws Exception {
