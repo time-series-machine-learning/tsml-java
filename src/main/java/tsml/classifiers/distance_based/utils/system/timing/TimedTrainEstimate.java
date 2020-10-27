9f08728dc8310c4e212fd24a4afcc1209e25d45a
George Oastler <goastler4@gmail.com>
2020-10-27 07:26:07 +0000

    Adjusted timing interfaces

--- src/main/java/tsml/classifiers/distance_based/utils/system/timing/TimedTrainEstimate.java
+++
@@ -1,9 +0,0 @@
-package tsml.classifiers.distance_based.utils.system.timing;
-
-public interface TimedTrainEstimate extends TrainEstimateTimeable {
-    StopWatch getTrainEstimateTimer();
-
-    default long getTrainEstimateTime() {
-        return getTrainEstimateTimer().getTime();
-    }
-}
