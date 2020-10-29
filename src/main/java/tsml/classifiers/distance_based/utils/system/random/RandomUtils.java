158035ceddd197bb8259b42af6db7f2280a86fb4
George Oastler <goastler4@gmail.com>
2020-10-29 13:39:55 +0000

    Random picking

--- src/main/java/tsml/classifiers/distance_based/utils/system/random/RandomUtils.java
+++ src/main/java/tsml/classifiers/distance_based/utils/system/random/RandomUtils.java
@@ -4,9 +4,11 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 import org.junit.Assert;
+import tsml.classifiers.distance_based.utils.collections.CollectionUtils;
 import tsml.classifiers.distance_based.utils.collections.iteration.BaseRandomIterator;
 import tsml.classifiers.distance_based.utils.collections.iteration.RandomIterator;
 import utilities.ArrayUtilities;
+import utilities.Utilities;
 
 import static tsml.classifiers.distance_based.utils.collections.CollectionUtils.newArrayList;
 
@@ -21,6 +23,7 @@ public class RandomUtils {
             RandomIterator<A> iterator, int numChoices) {
         final ArrayList<A> choices = new ArrayList<>(numChoices);
         for(int i = 0; i < numChoices; i++) {
+            if(!iterator.hasNext()) throw new IllegalStateException("iterator has no items remaining at iteration step " + i);
             choices.add(iterator.next());
         }
         return choices;
@@ -33,9 +36,10 @@ public class RandomUtils {
      * @param numChoices the number of choices to make
      * @param withReplacement whether to allow indices to be picked more than once
      * @param skipSingleOption whether to short circuit choices from lists of size 1
+     * @param allowReordering whether to allow the indices to be reordered during selection without replacement. This makes choosing from a large list faster, but does not maintain the ordering of list for efficiency purposes. Reordering whilst choosing is only concerned with the order of the indices left to choose from and does not reorder the list itself.
      * @return
      */
-    public static ArrayList<Integer> choiceIndex(int size, Random random, int numChoices, boolean withReplacement, boolean skipSingleOption) {
+    public static ArrayList<Integer> choiceIndex(int size, Random random, int numChoices, boolean withReplacement, boolean skipSingleOption, boolean allowReordering) {
         Assert.assertTrue(size > 0);
         if(skipSingleOption && size == 1) {
             return newArrayList(0);
@@ -44,11 +48,15 @@ public class RandomUtils {
         if(numChoices == 1) {
             return newArrayList(random.nextInt(size));
         }
+        if(!withReplacement && numChoices > size) {
+            throw new IllegalArgumentException("cannot choose " + numChoices + " from 0.." + size + " without replacement");
+        }
         final List<Integer> indices = ArrayUtilities.sequence(size);
         final BaseRandomIterator<Integer>
                 iterator = new BaseRandomIterator<>();
         iterator.setWithReplacement(withReplacement);
         iterator.setSkipSingleOption(skipSingleOption);
+        iterator.setAllowReordering(allowReordering);
         iterator.setRandom(random);
         iterator.buildIterator(indices);
         return choice(iterator, numChoices);
@@ -57,58 +65,100 @@ public class RandomUtils {
     // choice elements by index
 
     public static Integer choiceIndex(int size, Random random) {
-        return choiceIndex(size, random, 1, false, true).get(0);
+        return choiceIndex(size, random, 1, false, true, true).get(0);
     }
 
     public static Integer choiceIndexWithNoSkip(int size, Random random) {
-        return choiceIndex(size, random, 1, false, false).get(0);
+        return choiceIndex(size, random, 1, false, false, true).get(0);
     }
 
     public static ArrayList<Integer> choiceIndexWithReplacement(int size, Random random, int numChoices) {
-        return choiceIndex(size, random, numChoices, true, true);
+        return choiceIndex(size, random, numChoices, true, true, true);
     }
 
     public static ArrayList<Integer> choiceIndexWithReplacementWithNoSkip(int size, Random random, int numChoices) {
-        return choiceIndex(size, random, numChoices, true, false);
+        return choiceIndex(size, random, numChoices, true, false, true);
     }
 
     public static ArrayList<Integer> choiceIndex(int size, Random random, int numChoices) {
-        return choiceIndex(size, random, numChoices, false, true);
+        return choiceIndex(size, random, numChoices, false, true, true);
     }
 
     public static ArrayList<Integer> choiceIndexWithNoSkip(int size, Random random, int numChoices) {
-        return choiceIndex(size, random, numChoices, false, false);
+        return choiceIndex(size, random, numChoices, false, false, true);
     }
 
     // choose elements directly
 
-    public static <A> ArrayList<A> choice(List<A> list, Random random, int numChoices, boolean withReplacement, boolean skipIfSingleElement) {
-        return choiceIndex(list.size(), random, numChoices, withReplacement, skipIfSingleElement).stream().map(
-                list::get).collect(Collectors.toCollection(ArrayList::new));
+    public static <A> ArrayList<A> choice(List<A> list, Random random, int numChoices, boolean withReplacement, boolean skipIfSingleElement, boolean allowReordering) {
+        return Utilities.apply( choiceIndex(list.size(), random, numChoices, withReplacement, skipIfSingleElement, allowReordering), list::get);
     }
 
     public static <A> A choice(List<A> list, Random random) {
-        return choice(list, random, 1, false, true).get(0);
+        return choice(list, random, 1, false, false, true).get(0);
     }
 
     public static <A> A choiceWithNoSkip(List<A> list, Random random) {
-        return choice(list, random, 1, false, false).get(0);
+        return choice(list, random, 1, false, false, true).get(0);
     }
 
     public static <A> ArrayList<A> choiceWithReplacement(List<A> list, Random random, int numChoices) {
-        return choice(list, random, numChoices, true, true);
+        return choice(list, random, numChoices, true, true, true);
     }
 
     public static <A> ArrayList<A> choiceWithReplacementWithNoSkip(List<A> list, Random random, int numChoices) {
-        return choice(list, random, numChoices, true, false);
+        return choice(list, random, numChoices, true, false, true);
     }
 
     public static <A> ArrayList<A> choice(List<A> list, Random random, int numChoices) {
-        return choice(list, random, numChoices, false, true);
+        return choice(list, random, numChoices, false, true, true);
     }
 
     public static <A> ArrayList<A> choiceWithNoSkip(List<A> list, Random random, int numChoices) {
-        return choice(list, random, numChoices, false, false);
+        return choice(list, random, numChoices, false, false, true);
     }
 
+    // pick elements from list as well as choosing (i.e. make choice of elements and remove from the source list)
+
+    /**
+     * 
+     * @param list
+     * @param random
+     * @param numChoices
+     * @param withReplacement
+     * @param skipIfSingleElement
+     * @param allowReordering conversely to choice, this will reorder the list when picking >1 item and this parameter is true.
+     * @param <A>
+     * @return
+     */
+    public static <A> ArrayList<A> pick(List<A> list, Random random, int numChoices, boolean withReplacement, boolean skipIfSingleElement, boolean allowReordering) {
+        final ArrayList<Integer> indices = choiceIndex(list.size(), random, numChoices, withReplacement, skipIfSingleElement, allowReordering);
+        final ArrayList<A> chosen = Utilities.apply(indices, list::get);
+        CollectionUtils.removeAll(list, indices, allowReordering);
+        return chosen;
+    }
+    
+    public static <A> A pick(List<A> list, Random random) {
+        return pick(list, random, 1, false, true, true).get(0);
+    }
+
+    public static <A> A pickWithNoSkip(List<A> list, Random random) {
+        return pick(list, random, 1, false, false, true).get(0);
+    }
+
+    public static <A> ArrayList<A> pick(List<A> list, Random random, int numChoices) {
+        return pick(list, random, numChoices, false, true, true);
+    }
+
+    public static <A> ArrayList<A> pickWithNoSkip(List<A> list, Random random, int numChoices) {
+        return pick(list, random, numChoices, false, false, true);
+    }
+
+    public static <A> ArrayList<A> pickWithReplacement(List<A> list, Random random, int numChoices) {
+        return pick(list, random, numChoices, true, true, true);
+    }
+
+    public static <A> ArrayList<A> pickWithReplacementWithNoSkip(List<A> list, Random random, int numChoices) {
+        return pick(list, random, numChoices, true, false, true);
+    }
 }
--- src/main/java/tsml/classifiers/distance_based/utils/system/random/RandomUtilsTest.java
+++ src/main/java/tsml/classifiers/distance_based/utils/system/random/RandomUtilsTest.java
@@ -3,16 +3,20 @@ package tsml.classifiers.distance_based.utils.system.random;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import utilities.ArrayUtilities;
 import weka.core.Debug;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static tsml.classifiers.distance_based.utils.collections.CollectionUtils.newArrayList;
 
 public class RandomUtilsTest {
+
 
     private List<Integer> list;
     private Random random;
@@ -20,7 +24,7 @@ public class RandomUtilsTest {
     @Before
     public void before() {
         random = new Random(0);
-        list = newArrayList(1,2,3,4,5,6,7,8,9,10);
+        list = IntStream.rangeClosed(1,10).boxed().collect(Collectors.toList());
     }
 
     @Test
@@ -35,6 +39,35 @@ public class RandomUtilsTest {
         Assert.assertEquals(8, choice);
         choice = RandomUtils.choice(list, random);
         Assert.assertEquals(6, choice);
+        Assert.assertEquals(10, list.size());
+    }
+
+    @Test
+    public void testRandomPickSingle() {
+        int choice = RandomUtils.pick(list, random);
+        Assert.assertEquals(1, choice);
+        Assert.assertFalse(list.contains(1));
+        Assert.assertEquals(9, list.size());
+
+        choice = RandomUtils.pick(list, random);
+        Assert.assertEquals(8, choice);
+        Assert.assertFalse(list.contains(8));
+        Assert.assertEquals(8, list.size());
+
+        choice = RandomUtils.pick(list, random);
+        Assert.assertEquals(2, choice);
+        Assert.assertFalse(list.contains(2));
+        Assert.assertEquals(7, list.size());
+
+        choice = RandomUtils.pick(list, random);
+        Assert.assertEquals(3, choice);
+        Assert.assertFalse(list.contains(3));
+        Assert.assertEquals(6, list.size());
+
+        choice = RandomUtils.pick(list, random);
+        Assert.assertEquals(6, choice);
+        Assert.assertFalse(list.contains(6));
+        Assert.assertEquals(5, list.size());
     }
 
     @Test
@@ -42,10 +75,63 @@ public class RandomUtilsTest {
         final List<Integer> choice = RandomUtils.choice(list, random, 5);
         Assert.assertEquals(5, choice.size());
         Assert.assertEquals(new Integer(1), choice.get(0));
-        Assert.assertEquals(new Integer(9), choice.get(1));
-        Assert.assertEquals(new Integer(3), choice.get(2));
-        Assert.assertEquals(new Integer(5), choice.get(3));
-        Assert.assertEquals(new Integer(10), choice.get(4));
+        Assert.assertEquals(new Integer(8), choice.get(1));
+        Assert.assertEquals(new Integer(2), choice.get(2));
+        Assert.assertEquals(new Integer(3), choice.get(3));
+        Assert.assertEquals(new Integer(6), choice.get(4));
+        Assert.assertEquals(10, list.size());
+    }
+
+    @Test
+    public void testRandomPickMultiple() {
+        final List<Integer> choice = RandomUtils.pick(list, random, 5);
+        Assert.assertEquals(5, choice.size());
+        Assert.assertEquals(new Integer(1), choice.get(0));
+        Assert.assertEquals(new Integer(8), choice.get(1));
+        Assert.assertEquals(new Integer(2), choice.get(2));
+        Assert.assertEquals(new Integer(3), choice.get(3));
+        Assert.assertEquals(new Integer(6), choice.get(4));
+        Assert.assertTrue(list.contains(4));
+        Assert.assertTrue(list.contains(5));
+        Assert.assertTrue(list.contains(7));
+        Assert.assertTrue(list.contains(9));
+        Assert.assertTrue(list.contains(10));
+        Assert.assertTrue(list.size() == 5);
+    }
+
+    @Test
+    public void testRandomChoiceAll() {
+        final List<Integer> choice = RandomUtils.choice(list, random, 10);
+        Assert.assertEquals(list.size(), choice.size());
+        Assert.assertEquals(new Integer(1), choice.get(0));
+        Assert.assertEquals(new Integer(8), choice.get(1));
+        Assert.assertEquals(new Integer(2), choice.get(2));
+        Assert.assertEquals(new Integer(3), choice.get(3));
+        Assert.assertEquals(new Integer(6), choice.get(4));
+        Assert.assertEquals(new Integer(4), choice.get(5));
+        Assert.assertEquals(new Integer(7), choice.get(6));
+        Assert.assertEquals(new Integer(10), choice.get(7));
+        Assert.assertEquals(new Integer(9), choice.get(8));
+        Assert.assertEquals(new Integer(5), choice.get(9));
+        Assert.assertEquals(10, list.size());
+    }
+
+    @Test
+    public void testRandomPickAll() {
+        final List<Integer> choice = RandomUtils.pick(list, random, 10);
+        Assert.assertEquals(0, list.size());
+        Assert.assertEquals(10, choice.size());
+        Assert.assertEquals(new Integer(1), choice.get(0));
+        Assert.assertEquals(new Integer(8), choice.get(1));
+        Assert.assertEquals(new Integer(2), choice.get(2));
+        Assert.assertEquals(new Integer(3), choice.get(3));
+        Assert.assertEquals(new Integer(6), choice.get(4));
+        Assert.assertEquals(new Integer(4), choice.get(5));
+        Assert.assertEquals(new Integer(7), choice.get(6));
+        Assert.assertEquals(new Integer(10), choice.get(7));
+        Assert.assertEquals(new Integer(9), choice.get(8));
+        Assert.assertEquals(new Integer(5), choice.get(9));
+        Assert.assertTrue(list.isEmpty());
     }
 
     @Test
@@ -53,6 +139,7 @@ public class RandomUtilsTest {
         final List<Integer> choice = RandomUtils.choiceWithReplacement(list, random, list.size() * 10);
         final Set<Integer> set = new HashSet<>(choice);
         Assert.assertEquals(list.size(), set.size());
+        Assert.assertEquals(10, list.size());
     }
 
     @Test
@@ -60,10 +147,28 @@ public class RandomUtilsTest {
         final List<Integer> choice = RandomUtils.choice(list, random, 10);
         final Set<Integer> set = new HashSet<>(choice);
         Assert.assertEquals(choice.size(), set.size());
+        Assert.assertEquals(10, list.size());
+    }
+
+    @Test
+    public void testRandomPickWithReplacement() {
+        final List<Integer> choice = RandomUtils.pickWithReplacement(list, random, list.size() * 10);
+        final Set<Integer> set = new HashSet<>(choice);
+        Assert.assertEquals(10, set.size());
+        Assert.assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testRandomPickWithoutReplacement() {
+        final List<Integer> choice = RandomUtils.pick(list, random, 10);
+        final Set<Integer> set = new HashSet<>(choice);
+        Assert.assertEquals(choice.size(), set.size());
+        Assert.assertEquals(0, list.size());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testRandomChoiceTooMany() {
         RandomUtils.choice(list, random, list.size() + 1);
     }
+
 }
