54455ff1491ba3a6e908f4aa099415116ce569dd
George Oastler <goastler4@gmail.com>
2020-11-21 01:54:43 +0000

    made timestamp better with individual unit access and timestamp option

--- src/main/java/tsml/classifiers/distance_based/utils/experiment/TimeSpan.java
+++ src/main/java/tsml/classifiers/distance_based/utils/experiment/TimeSpan.java
@@ -1,34 +1,34 @@
 package tsml.classifiers.distance_based.utils.experiment;
 
+import tsml.classifiers.distance_based.utils.strings.StrUtils;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.*;
+
 public class TimeSpan implements Comparable<TimeSpan> {
     
-    public static final String NONE = "none";
-    
-    public TimeSpan(final String inputLabel) {
-        if(inputLabel.equalsIgnoreCase(NONE)) {
-            nanos = -1;
-            isNone = true;
-        } else {
-            // convert the label to nanos
-            nanos = strToNanos(inputLabel);
-            isNone = false;
-        }
+    public TimeSpan(TimeSpan other) {
+        this(other.inNanos());
     }
     
-    public static String nanosToStr(long nanos) {
-        final StringBuilder sb = new StringBuilder();
+    public TimeSpan(long nanos) {
+        this.nanos = nanos;
+        if(nanos < 0) {
+            nanos = Math.abs(nanos);
+        }
         long remainingAmount = nanos;
         final TimeUnit[] values = TimeUnit.values();
+        amounts = new long[TimeUnit.values().length];
         for(int i = values.length - 1; i >= 0; i--) {
             final TimeUnit unit = values[i];
             final long amount = unit.convert(remainingAmount, TimeUnit.NANOSECONDS);
+            amounts[i] = amount;
             if(amount > 0) {
-                sb.append(amount).append(unitToStr(unit));
                 // subtract the amount of time this accounts for in the remaining time. I.e. if the remaining time 
                 // was 1.5 days (in nanos) then the amount would be only 1 day. We need to subtract the 1 day from 
                 // the 1.5 days to give .5 days which can be represented in smaller units, i.e. hours.
@@ -39,21 +39,30 @@ public class TimeSpan implements Comparable<TimeSpan> {
                 }
             }
         }
-        return sb.toString();
     }
     
-    public static long strToNanos(String label) {
+    public TimeSpan(final String label) {
+        this(labelToNanos(label));
+    }
+    
+    private static long labelToNanos(String label) {
         if(label.isEmpty()) {
             throw new IllegalArgumentException("empty input");
+        }
+        boolean negative = false;
+        int i = 0;
+        if(label.charAt(0) == '-') {
+            negative = true;
+            i++;
         }
         StringBuilder sb = new StringBuilder();
         long totalNanos = 0;
         List<String> parts = new ArrayList<>();
         boolean previousIsDigit = true; // whether the last char was a digit or not
-        for(int i = 0; i < label.length(); i++) {
+        for(; i < label.length(); i++) {
             final char c = label.charAt(i);
-            final boolean isDigit = isDigit(c);
-            final boolean isAlpha = isAlpha(c);
+            final boolean isDigit = StrUtils.isDigit(c);
+            final boolean isAlpha = StrUtils.isAlpha(c);
             if(!isAlpha && !isDigit) {
                 throw new IllegalArgumentException(c + " is not a digit or alpha character");
             }
@@ -74,27 +83,22 @@ public class TimeSpan implements Comparable<TimeSpan> {
         if(parts.size() % 2 != 0) {
             throw new IllegalArgumentException("odd number of parts to string: " + label);
         }
-        for(int i = 0; i < parts.size(); i += 2) {
-            final long value = Long.parseLong(parts.get(i));
-            final String unit = parts.get(i + 1);
+        for(int j = 0; j < parts.size(); j += 2) {
+            final long value = Long.parseLong(parts.get(j));
+            final String unit = parts.get(j + 1);
             totalNanos += TimeUnit.NANOSECONDS.convert(value, strToUnit(unit));
+        }
+        if(negative) {
+            totalNanos = -totalNanos;
         }
         return totalNanos;
     }
     
-    public static boolean isAlpha(char c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-    }
-    
-    public static boolean isDigit(char c) {
-        return (c >= '0' && c <= '9');
-    }
-    
     public static TimeUnit strToUnit(String str) {
         switch(str.toLowerCase()) {
-            case "d": return TimeUnit.DAYS;
-            case "h": return TimeUnit.HOURS;
-            case "m": return TimeUnit.MINUTES;
+            case "d": return DAYS;
+            case "h": return HOURS;
+            case "m": return MINUTES;
             case "s": return TimeUnit.SECONDS;
             case "ms": return TimeUnit.MILLISECONDS;
             case "us": return TimeUnit.MICROSECONDS;
@@ -118,40 +122,98 @@ public class TimeSpan implements Comparable<TimeSpan> {
 
     private String label;
     private final long nanos;
-    private boolean isNone;
+    private final long[] amounts;
 
-    public long inNanos() {
-        return nanos;
+    /**
+     * Get the specific amount of a given unit. I.e. take 1h33m50s. days() would give 0, hours() would give 1, minutes() would give 33 and seconds() would give 50.
+     * @param unit
+     * @return
+     */
+    public long get(TimeUnit unit) {
+        return amounts[unit.ordinal()];
     }
     
-    public boolean isNone() {
-        return isNone;
+    public long days() {
+        return get(DAYS);
+    }
+    
+    public long hours() {
+        return get(HOURS);
+    }
+
+    public long minutes() {
+        return get(MINUTES);
+    }
+
+    public long seconds() {
+        return get(TimeUnit.SECONDS);
+    }
+
+    public long milliseconds() {
+        return get(TimeUnit.MILLISECONDS);
+    }
+
+    public long microseconds() {
+        return get(TimeUnit.MICROSECONDS);
+    }
+
+    public long nanoseconds() {
+        return get(TimeUnit.NANOSECONDS);
+    }
+
+    /**
+     * The time span expressed in nanoseconds.
+     * @return
+     */
+    public long inNanos() {
+        return nanos;
     }
     
     public String label() {
         if(label == null) {
             // the input label may be in an odd format, i.e. 90m. Therefore, convert the nanos back to a label which will output in maximised units, i.e. 1h30m. Use the nanos representation to do this.
-            label = nanosToStr(nanos);
+            StringBuilder sb = new StringBuilder();
+            if(nanos < 0) {
+                sb.append("-");
+            }
+            for(int i = TimeUnit.values().length - 1; i >= 0; i--) {
+                TimeUnit unit = TimeUnit.values()[i];
+                long amount = get(unit);
+                if(amount > 0) {
+                    sb.append(amount).append(unitToStr(unit));
+                }
+            }
+            label = sb.toString();
         }
         return label;
     }
 
+    public String asTimeStamp() {
+        StringBuilder sb = new StringBuilder();
+        if(nanos < 0) {
+            sb.append("-");
+        }
+        boolean first = true;
+        for(int i = values().length - 1; i >= MINUTES.ordinal(); i--) {
+            TimeUnit unit = values()[i];
+            long amount = get(unit);
+            if(!first) {
+                sb.append(String.format("%02d", amount));
+            } else if(amount > 0 || unit.equals(MINUTES)) {
+                sb.append(amount).append(":");
+                first = false;
+            }
+        }
+        sb.append(String.format("%02d", seconds())).append(".").append(String.format("%03d", milliseconds()));
+        return sb.toString();
+    }
+
     @Override public String toString() {
-        return label();
+        return asTimeStamp();
     }
 
     @Override public int compareTo(final TimeSpan other) {
-        final boolean otherNone = other.isNone();
-        final boolean none = isNone();
-        if(otherNone && none) {
-            return 0;
-        } else if(otherNone) {
-            return 1;
-        } else if(none) {
-            return -1;
-        } else {
-            return Long.compare(inNanos(), other.inNanos());
-        }
+        return Long.compare(inNanos(), other.inNanos());
     }
 
     @Override public boolean equals(final Object o) {
--- src/main/java/tsml/classifiers/distance_based/utils/experiment/TimeSpanTest.java
+++ src/main/java/tsml/classifiers/distance_based/utils/experiment/TimeSpanTest.java
@@ -12,96 +12,97 @@ import static tsml.classifiers.distance_based.utils.collections.CollectionUtils.
 public class TimeSpanTest {
     
     @Test
+    public void testTimeSpanConversion() {
+        Assert.assertEquals(130000000000L, new TimeSpan(130000000000L).inNanos());
+        Assert.assertEquals(-130000000000L, new TimeSpan(-130000000000L).inNanos());
+        Assert.assertEquals(130000000000L, new TimeSpan("130s").inNanos());
+        Assert.assertEquals(130000000000L, new TimeSpan("1m70s").inNanos());
+        Assert.assertEquals(130000000000L, new TimeSpan("2m10s").inNanos());
+        Assert.assertEquals(-130000000000L, new TimeSpan("-130s").inNanos());
+        Assert.assertEquals(-130000000000L, new TimeSpan("-1m70s").inNanos());
+        Assert.assertEquals(-130000000000L, new TimeSpan("-2m10s").inNanos());
+        Assert.assertEquals("0:00:02:10", new TimeSpan("130s").asTimeStamp());
+        Assert.assertEquals("-0:00:02:10", new TimeSpan("-130s").asTimeStamp());
+    }
+    
+    @Test
     public void testSort() {
         final List<String> output =
-                newArrayList("none", "130s", "2m10s", "71s", "1m000001s", "none").stream().map(TimeSpan::new).sorted()
+                newArrayList("130s", "2m10s", "71s", "1m000001s").stream().map(TimeSpan::new).sorted()
                         .map(TimeSpan::toString).collect(Collectors.toList());
-        final ArrayList<String> expected = newArrayList("1m1s", "1m11s", "2m10s", "2m10s", "none", "none");
+        final ArrayList<String> expected = newArrayList("1m1s", "1m11s", "2m10s", "2m10s");
         Assert.assertEquals(expected, output);
     }
     
     @Test
-    public void testNone() {
-        final TimeSpan none = new TimeSpan("none");
-        final TimeSpan some = new TimeSpan("70s");
-        Assert.assertEquals(-1, none.inNanos());
-        Assert.assertTrue(none.isNone());
-        Assert.assertEquals(70000000000L, some.inNanos());
-        Assert.assertFalse(some.isNone());
-        Assert.assertEquals(0, none.compareTo(new TimeSpan("none")));
-        Assert.assertTrue(none.compareTo(some) < 0);
-        Assert.assertTrue(some.compareTo(none) > 0);
-    }
-    
-    @Test
     public void testStrToNanos1m70s() {
-        Assert.assertEquals(130000000000L, TimeSpan.strToNanos("1m70s"));
+        Assert.assertEquals(130000000000L, new TimeSpan("1m70s").inNanos());
     }
     
     @Test
     public void testNanosToStr1m70s() {
-        Assert.assertEquals("2m10s", TimeSpan.nanosToStr(130000000000L));
+        Assert.assertEquals("2m10s", new TimeSpan(130000000000L).label());
     }
 
     @Test
     public void testStrToNanos130s() {
-        Assert.assertEquals(130000000000L, TimeSpan.strToNanos("130s"));
+        Assert.assertEquals(130000000000L, new TimeSpan("130s").inNanos());
     }
 
     @Test
     public void testNanosToStr130s() {
-        Assert.assertEquals("2m10s", TimeSpan.nanosToStr(130000000000L));
+        Assert.assertEquals("2m10s", new TimeSpan(130000000000L).label());
     }
     
     @Test
     public void testEquals() {
-        Assert.assertEquals(TimeSpan.nanosToStr(130000000000L), TimeSpan.nanosToStr(130000000000L));
-        Assert.assertNotEquals(TimeSpan.nanosToStr(130000000000L), TimeSpan.nanosToStr(130000000001L));
-        Assert.assertEquals(TimeSpan.strToNanos("1h30m"), TimeSpan.strToNanos("90m"));
-        Assert.assertEquals(TimeSpan.strToNanos("2m10s"), TimeSpan.strToNanos("130s"));
-        Assert.assertEquals(TimeSpan.strToNanos("2m10s"), TimeSpan.strToNanos("1m70s"));
-        Assert.assertEquals(TimeSpan.strToNanos("130s"), TimeSpan.strToNanos("1m70s"));
-        Assert.assertNotEquals(TimeSpan.strToNanos("131s"), TimeSpan.strToNanos("1m70s"));
-        Assert.assertEquals(TimeSpan.strToNanos("130s"), TimeSpan.strToNanos(TimeSpan.nanosToStr(TimeSpan.strToNanos("1m70s"))));
+        Assert.assertEquals(new TimeSpan(130000000000L), new TimeSpan(130000000000L));
+        Assert.assertNotEquals(new TimeSpan(130000000000L), new TimeSpan(130000000001L));
+        Assert.assertEquals(new TimeSpan("1h30m"), new TimeSpan("90m"));
+        Assert.assertEquals(new TimeSpan("2m10s"), new TimeSpan("130s"));
+        Assert.assertEquals(new TimeSpan("2m10s"), new TimeSpan("1m70s"));
+        Assert.assertEquals(new TimeSpan("130s"), new TimeSpan("1m70s"));
+        Assert.assertNotEquals(new TimeSpan("131s"), new TimeSpan("1m70s"));
+        Assert.assertEquals(new TimeSpan("130s"), new TimeSpan(new TimeSpan(new TimeSpan("1m70s"))));
     }
     
     @Test
     public void testNanosToStr1h() {
-        Assert.assertEquals("1h", TimeSpan.nanosToStr(3600000000000L));
+        Assert.assertEquals("1h", new TimeSpan(3600000000000L).label());
     }
 
     @Test
     public void testNanosToStr90m() {
-        Assert.assertEquals("1h30m", TimeSpan.nanosToStr(5400000000000L));
+        Assert.assertEquals("1h30m", new TimeSpan(5400000000000L).label());
     }
 
     @Test
     public void testNanosToStr26h87m102s() {
-        Assert.assertEquals("1d3h28m42s", TimeSpan.nanosToStr(98922000000000L));
+        Assert.assertEquals("1d3h28m42s", new TimeSpan(98922000000000L).label());
     }
 
     @Test
     public void testStrToNanos1h() {
-        Assert.assertEquals(3600000000000L, TimeSpan.strToNanos("1h"));
+        Assert.assertEquals(3600000000000L, new TimeSpan("1h").inNanos());
     }
 
     @Test
     public void testStrToNanos90m() {
-        Assert.assertEquals(5400000000000L, TimeSpan.strToNanos("90m"));
+        Assert.assertEquals(5400000000000L, new TimeSpan("90m").inNanos());
     }
 
     @Test
     public void testStrToNanos26h87m102s() {
-        Assert.assertEquals(98922000000000L, TimeSpan.strToNanos("26h87m102s"));
+        Assert.assertEquals(98922000000000L, new TimeSpan("26h87m102s").inNanos());
     }
     
     @Test(expected = IllegalArgumentException.class)
     public void testStrToNanosInvalidUnit() {
-        TimeSpan.strToNanos("26z");
+        new TimeSpan("26z");
     }
     
     @Test(expected = IllegalArgumentException.class)
     public void testStrToNanosEmpty() {
-        TimeSpan.strToNanos("");
+        new TimeSpan("");
     }
 }
