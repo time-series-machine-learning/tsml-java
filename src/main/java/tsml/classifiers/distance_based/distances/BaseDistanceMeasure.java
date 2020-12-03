caad9b15d610da6ac8361ab43cbe4e92aa932c7e
George Oastler <goastler4@gmail.com>
2020-12-03 09:12:18 +0000

    Adjusted distance measure structuring

--- src/main/java/tsml/classifiers/distance_based/distances/BaseDistanceMeasure.java
+++ src/main/java/tsml/classifiers/distance_based/distances/BaseDistanceMeasure.java
@@ -9,6 +9,8 @@ import weka.core.Instances;
 import weka.core.Utils;
 import weka.core.neighboursearch.PerformanceStats;
 
+import java.util.Objects;
+
 /**
  * Abstract distance measure. This takes the weka interface for DistanceFunction and implements some default methods,
  * adding several checks and balances also. All distance measures should extends this class. This is loosely based on
@@ -20,181 +22,30 @@ import weka.core.neighboursearch.PerformanceStats;
  */
 public abstract class BaseDistanceMeasure implements DistanceMeasure {
 
-    public BaseDistanceMeasure() {
-        setName(getClass().getSimpleName());
-        setLongestInstanceFirst(true);
-        setStoreData(false);
-        setInvertSelection(false);
+    private boolean generateDistanceMatrix = false;
+    // the distance matrix produced by the distance function
+    private double[][] matrix;
+
+    public double[][] getDistanceMatrix() {
+        return matrix;
     }
 
-    // check for whether setInstances has been called before doing any distance measurements
-    private boolean fitted;
-    // whether to set the first instance to the longest instance when calling the distance function
-    private boolean longestInstanceFirst;
-    // the name of this distance measure
-    private String name;
-    // whether to store the fitted data
-    private boolean storeData;
-    // the stored data
-    private Instances data;
-    // whether to invert the distance measure. false --> the smaller the distance the more similar. true --> the
-    // larger the distance the more similar
-    private boolean invert;
-
-    @Override
-    public String toString() {
-        return getName() + " " + Utils.joinOptions(getOptions());
-    }
-
-    /**
-     * clean up any data collected during fitting
-     */
-    public void clean() {
-        fitted = false;
-        data = null;
-    }
-
-    public boolean isLongestInstanceFirst() {
-        return longestInstanceFirst;
-    }
-
-    protected void setLongestInstanceFirst(final boolean longestInstanceFirst) {
-        this.longestInstanceFirst = longestInstanceFirst;
-    }    @Override
-    public double getMaxDistance() {
-        return Double.POSITIVE_INFINITY;
-    }
-
-    /**
-     * whether the fit method has been called
-     *
-     * @return
-     */
-    public boolean isFitted() {
-        return fitted;
-    }
-
-    private void setFitted(final boolean fitted) {
-        this.fitted = fitted;
-    }    /**
-     * whether the distance measure is symmetric, i.e. dist(a,b) == dist(b,a)
-     *
-     * @return
-     */
-    public boolean isSymmetric() {
-        return true;
-    }
-
-    @Override public Instances getInstances() {
-        return data;
-    }
-
-    @Override
-    public final double distance(final Instance a, final Instance b) {
-        return distance(a, b, getMaxDistance());
-    }
-
-    @Override
-    public final double distance(Instance a, Instance b, final double limit) {
-        // make sure class labels are at the end
-        Assert.assertEquals(a.numAttributes() - 1, a.classIndex());
-        Assert.assertEquals(b.numAttributes() - 1, b.classIndex());
-        // put a as the longest time series
-        if(longestInstanceFirst && b.numAttributes() > a.numAttributes()) {
-            Instance tmp = a;
-            a = b;
-            b = tmp;
-        }
-        return findDistance(a, b, limit);
-    }
-
-    protected abstract double findDistance(final Instance a, final Instance b, final double limit);
-
-    @Override
-    public final double distance(final Instance a, final Instance b, final double limit, final PerformanceStats stats) {
-        return distance(a, b, limit);
-    }
-
-    @Override
-    public void postProcessDistances(final double[] distances) {
-
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override public void setName(final String name) {
-        Assert.assertNotNull(name);
-        this.name = name;
-    }
-
-    @Override
-    public final double distance(final Instance a, final Instance b, final PerformanceStats stats) throws Exception {
-        return distance(a, b, getMaxDistance(), stats);
-    }
-
-    @Override
-    public void setAttributeIndices(final String value) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public String getAttributeIndices() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void setInvertSelection(final boolean value) {
-        invert = value;
-    }
-
-    @Override
-    public boolean getInvertSelection() {
-        return invert;
-    }
-
-    /**
-     * fit the distance measure to this data. This is the equivalent of fit()
-     *
-     * @param data
-     */
-    public void setInstances(Instances data) {
-        Assert.assertNotNull(data);
-        if(storeData) {
-            this.data = data;
-        }
-        fitted = true;
-        if(data.classIndex() != data.numAttributes() - 1) {
-            throw new IllegalStateException("class value must be at the end");
+    protected void setDistanceMatrix(double[][] matrix) {
+        if(isGenerateDistanceMatrix()) {
+            this.matrix = matrix;
         }
     }
 
-    @Override
-    public void update(final Instance ins) {
-        if(!fitted) {
-            throw new IllegalStateException("not fitted");
-        }
-        if(storeData) {
-            data.add(ins);
-        }
+    public void cleanDistanceMatrix() {
+        matrix = null;
     }
 
-    @Override public void setParams(final ParamSet param) throws Exception {
-
+    public boolean isGenerateDistanceMatrix() {
+        return generateDistanceMatrix;
     }
 
-    @Override public ParamSet getParams() {
-        return new ParamSet();
-    }
-
-    public boolean isStoreData() {
-        return storeData;
-    }
-
-    public void setStoreData(final boolean storeData) {
-        this.storeData = storeData;
+    public void setGenerateDistanceMatrix(final boolean generateDistanceMatrix) {
+        this.generateDistanceMatrix = generateDistanceMatrix;
     }
 
 }
--- src/main/java/tsml/classifiers/distance_based/distances/DistanceMeasure.java
+++ src/main/java/tsml/classifiers/distance_based/distances/DistanceMeasure.java
@@ -14,44 +14,86 @@ public interface DistanceMeasure extends Serializable, DistanceFunction, ParamHa
 
     String DISTANCE_MEASURE_FLAG = "d";
 
-    // the maximum distance the distance measure could produce
-    double getMaxDistance();
-
     // whether the distance measure is symmetric (i.e. dist from inst A to inst B == dist from inst B to inst A
-    boolean isSymmetric();
+    default boolean isSymmetric() {
+        return true;
+    }
 
-    double distance(final Instance a, final Instance b);
+    default double distance(final Instance a, final Instance b) {
+        return distance(a, b, Double.POSITIVE_INFINITY);
+    }
+    
+    default double distance(final Instance a, final Instance b, PerformanceStats stats) {
+        return distance(a, b);
+    }
+    
+    default double distance(final Instance a, final Instance b, double limit, PerformanceStats stats) {
+        return distance(a, b, limit);
+    }
 
-    double distance(final Instance a, final Instance b, final double limit);
-
-    @Override
-    double distance(final Instance a, final Instance b, final PerformanceStats stats)
-        throws Exception;
+    /**
+     * Override this distance func
+     * @param a
+     * @param b
+     * @param limit
+     * @return
+     */
+    default double distance(final Instance a, final Instance b, final double limit) {
+        return distance(Converter.fromArff(a), Converter.fromArff(b), limit);
+    }
 
     default double distance(final TimeSeriesInstance a, final TimeSeriesInstance b) {
         return distance(a, b, Double.POSITIVE_INFINITY);
     }
-    
+
+    /**
+     * Or override this distance func
+     * @param a
+     * @param b
+     * @param limit
+     * @return
+     */
     default double distance(final TimeSeriesInstance a, final TimeSeriesInstance b, double limit) {
         return distance(Converter.toArff(a), Converter.toArff(b), limit);
     }
     
-    String getName();
-
-    void setName(String name);
-
-    // the fit function
-    void setInstances(Instances data);
-
-    default void setInstances(TimeSeriesInstances data) {
-        setInstances(Converter.toArff(data));
+    default String getName() {
+        return getClass().getSimpleName();
     }
 
-    static String getName(DistanceFunction df) {
-        if(df instanceof DistanceMeasure) {
-            return ((DistanceMeasure) df).getName();
-        } else {
-            return df.getClass().getSimpleName();
-        }
+    default void setInstances(Instances data) {
+        buildDistanceMeasure(Converter.fromArff(data));
+    }
+    
+    default void buildDistanceMeasure(TimeSeriesInstances data) {
+        
+    }
+    
+    default Instances getInstances() {
+        return null;
+    }
+
+    default void setAttributeIndices(String value) {
+        
+    }
+
+    default String getAttributeIndices() {
+        return null;
+    }
+
+    default void setInvertSelection(boolean value) {
+        
+    }
+
+    default boolean getInvertSelection() {
+        return false;
+    }
+
+    default void postProcessDistances(double[] distances) {
+        
+    }
+
+    default void update(Instance ins) {
+        
     }
 }
