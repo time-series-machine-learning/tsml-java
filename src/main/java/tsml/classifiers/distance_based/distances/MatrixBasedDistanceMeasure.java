df1e2b0fe1dfc2aff5f12a9f1984824fe0854455
George Oastler <goastler4@gmail.com>
2020-12-16 20:00:31 +0000

    Matrix template for distance measures

--- src/main/java/tsml/classifiers/distance_based/distances/MatrixBasedDistanceMeasure.java
+++ src/main/java/tsml/classifiers/distance_based/distances/MatrixBasedDistanceMeasure.java
@@ -21,7 +21,7 @@ import java.util.Objects;
  * <p>
  * Contributors: goastler
  */
-public abstract class BaseDistanceMeasure implements DistanceMeasure {
+public abstract class MatrixBasedDistanceMeasure implements DistanceMeasure {
 
     private boolean generateDistanceMatrix = false;
     // the distance matrix produced by the distance function
