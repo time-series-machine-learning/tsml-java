c5a5f271c7e5bcc340219765af26332f97911b38
George Oastler <goastler4@gmail.com>
2021-01-14 23:08:48 +0000

    Checked MSM and made mv case faster

--- src/main/java/tsml/classifiers/distance_based/distances/msm/MSMDistance.java
+++ src/main/java/tsml/classifiers/distance_based/distances/msm/MSMDistance.java
@@ -3,6 +3,7 @@ package tsml.classifiers.distance_based.distances.msm;
 import tsml.classifiers.distance_based.distances.MatrixBasedDistanceMeasure;
 import tsml.classifiers.distance_based.utils.collections.params.ParamHandlerUtils;
 import tsml.classifiers.distance_based.utils.collections.params.ParamSet;
+import tsml.data_containers.TimeSeries;
 import tsml.data_containers.TimeSeriesInstance;
 
 import java.util.Arrays;
@@ -64,12 +65,15 @@ public class MSMDistance extends MatrixBasedDistanceMeasure {
      * @return
      */
     private double cost(final TimeSeriesInstance a, final int aIndex, final TimeSeriesInstance b, final int bIndex, final TimeSeriesInstance c, final int cIndex) {
-        final double[] aSlice = a.getVSliceArray(aIndex);
-        final double[] bSlice = b.getVSliceArray(bIndex);
-        final double[] cSlice = c.getVSliceArray(cIndex);
         double sum = 0;
-        for(int i = 0; i < aSlice.length; i++) {
-            sum += findCost(aSlice[i], bSlice[i], cSlice[i]);
+        for(int i = 0; i < a.getNumDimensions(); i++) {
+            final TimeSeries aDim = a.get(i);
+            final TimeSeries bDim = b.get(i);
+            final TimeSeries cDim = c.get(i);
+            final Double aValue = aDim.get(aIndex);
+            final Double bValue = bDim.get(bIndex);
+            final Double cValue = cDim.get(cIndex);
+            sum += findCost(aValue, bValue, cValue);
         }
         return sum;
     }
@@ -83,93 +87,87 @@ public class MSMDistance extends MatrixBasedDistanceMeasure {
      * @return
      */
     private double directCost(final TimeSeriesInstance a, final int aIndex, final TimeSeriesInstance b, final int bIndex) {
-        final double[] aSlice = a.getVSliceArray(aIndex);
-        final double[] bSlice = b.getVSliceArray(bIndex);
-        final double[] result = subtract(aSlice, bSlice);
-        abs(result);
-        return sum(result);
+        double sum = 0;
+        for(int i = 0; i < a.getNumDimensions(); i++) {
+            final TimeSeries aDim = a.get(i);
+            final TimeSeries bDim = b.get(i);
+            final Double aValue = aDim.get(aIndex);
+            final Double bValue = bDim.get(bIndex);
+            sum += Math.abs(aValue - bValue);
+        }
+        return sum;
     }
 
     @Override
     public double distance(TimeSeriesInstance a, TimeSeriesInstance b, final double limit) {
-        // collect info
-        checkData(a, b, limit);
         final int aLength = a.getMaxLength();
         final int bLength = b.getMaxLength();
+        setup(aLength, bLength, true);
         final double lengthRatio = (double) bLength / aLength;
         final double windowSize = 1d * bLength;
+        
         // start and end of window
-        int start = 1; // start at 1 because 0th element is filled directly below
+        int start = 0;
+        int j = start;
+        int i = 0;
         double mid;
         int end =  (int) Math.min(bLength - 1, Math.ceil(windowSize));
         int prevEnd;
-        // setup matrix and rows
-        final boolean generateDistanceMatrix = isGenerateDistanceMatrix();
-        final double[][] matrix;
-        double[] row;
-        double[] prevRow = null;
-        if(generateDistanceMatrix) {
-            matrix = new double[aLength][bLength];
-            for(double[] array : matrix) Arrays.fill(array, Double.POSITIVE_INFINITY);
-            row = matrix[0];
-        } else {
-            matrix = null;
-            row = new double[bLength];
-            prevRow = new double[bLength];
-        }
-        setDistanceMatrix(matrix);
+        double[] row = getRow(i);
+        double[] prevRow;
+        
         // process top left sqaure of mat
-        double min = directCost(a, 0, b, 0);
-        row[0] = min;
+        double min = row[j] = directCost(a, i, b, j);
+        j++;
         // compute the first row
-        for(int j = start; j <= end; j++) {
-            double cost = row[j - 1] + cost(b, j, a, 0, b, j - 1);
-            row[j] = cost;
-            min = Math.min(min, cost);
+        for(; j <= end; j++) {
+            row[j] = row[j - 1] + cost(b, j, a, i, b, j - 1);
+            min = Math.min(min, row[j]);
         }
         if(min > limit) return Double.POSITIVE_INFINITY; // quit if beyond limit
+        i++;
+        
         // process remaining rows
-        for(int i = 1; i < aLength; i++) {
+        for(; i < aLength; i++) {
             // reset min for the row
             min = Double.POSITIVE_INFINITY;
+            
             // start, end and mid of window
             prevEnd = end;
             mid = i * lengthRatio;
             start = (int) Math.max(0, Math.floor(mid - windowSize));
+            j = start;
             end = (int) Math.min(bLength - 1, Math.ceil(mid + windowSize));
+            
             // change rows
-            if(generateDistanceMatrix) {
-                row = matrix[i];
-                prevRow = matrix[i - 1];
-            } else {
-                // reuse previous row
-                double[] tmp = row;
-                row = prevRow;
-                prevRow = tmp;
-                // set the top values outside of window to inf
-                Arrays.fill(prevRow, prevEnd + 1, end + 1, Double.POSITIVE_INFINITY);
-                // set the value left of the window to inf
-                if(start > 0) row[start - 1] = Double.POSITIVE_INFINITY;
-            }
+            prevRow = row;
+            row = getRow(i);
+            
+            // set the top values outside of window to inf
+            Arrays.fill(prevRow, prevEnd + 1, end + 1, Double.POSITIVE_INFINITY);
+            // set the value left of the window to inf
+            if(j > 0) row[j - 1] = Double.POSITIVE_INFINITY;
+            
             // if assessing the left most column then only mapping option is top - not left or topleft
-            if(start == 0) {
-                final double cost = prevRow[start] + cost(a, i, a, i - 1, b, start);
-                row[start++] = cost;
-                min = Math.min(min, cost);
+            if(j == 0) {
+                row[j] = prevRow[j] + cost(a, i, a, i - 1, b, j);
+                min = Math.min(min, row[j++]);
             }
+            
             // compute the distance for each cell in the row
-            for(int j = start; j <= end; j++) {
+            for(; j <= end; j++) {
                 final double topLeft = prevRow[j - 1] + directCost(a, i, b, j);
                 final double top = prevRow[j] + cost(a, i, a, i - 1, b, j);
                 final double left = row[j - 1] + cost(b, j, a, i, b, j - 1);
-                final double cost = Math.min(top, Math.min(left, topLeft));
-                row[j] = cost;
-                min = Math.min(min, cost);
+                row[j] = Math.min(top, Math.min(left, topLeft));
+                min = Math.min(min, row[j]);
             }
             if(min > limit) return Double.POSITIVE_INFINITY; // quit if beyond limit
         }
         // last value in the current row is the distance
-        return row[bLength - 1];
+        final double distance = row[row.length - 1];
+        teardown();
+        return distance;
     }
 
     @Override
