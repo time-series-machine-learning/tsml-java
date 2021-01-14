7484730a4d1a602539066715b53eb36a05db34b3
George Oastler <goastler4@gmail.com>
2021-01-14 23:08:39 +0000

    Checked LCSS and made mv case faster

--- src/main/java/tsml/classifiers/distance_based/distances/lcss/LCSSDistance.java
+++ src/main/java/tsml/classifiers/distance_based/distances/lcss/LCSSDistance.java
@@ -3,6 +3,7 @@ package tsml.classifiers.distance_based.distances.lcss;
 import tsml.classifiers.distance_based.distances.MatrixBasedDistanceMeasure;
 import tsml.classifiers.distance_based.utils.collections.params.ParamHandlerUtils;
 import tsml.classifiers.distance_based.utils.collections.params.ParamSet;
+import tsml.data_containers.TimeSeries;
 import tsml.data_containers.TimeSeriesInstance;
 
 import java.util.Arrays;
@@ -34,9 +35,12 @@ public class LCSSDistance extends MatrixBasedDistanceMeasure {
     }
 
     private boolean approxEqual(TimeSeriesInstance a, int aIndex, TimeSeriesInstance b, int bIndex) {
+        double sum = 0;
         for(int i = 0; i < a.getNumDimensions(); i++) {
-            final Double aValue = a.get(i).get(aIndex);
-            final Double bValue = b.get(i).get(bIndex);
+            final TimeSeries aDim = a.get(i);
+            final TimeSeries bDim = b.get(i);
+            final Double aValue = aDim.get(aIndex);
+            final Double bValue = bDim.get(bIndex);
             if(Math.abs(aValue - bValue) > epsilon) {
                 return false;
             }
@@ -46,12 +50,12 @@ public class LCSSDistance extends MatrixBasedDistanceMeasure {
 
     @Override
     public double distance(final TimeSeriesInstance a, final TimeSeriesInstance b, double limit) {
-        // collect info
-        checkData(a, b, limit);
         final int aLength = a.getMaxLength();
         final int bLength = b.getMaxLength();
+        setup(aLength, bLength, true);
         final double lengthRatio = (double) bLength / aLength;
         final double windowSize = this.windowSize * bLength;
+        
         // 22/10/19 goastler - limit LCSS such that if any value in the current window is larger than the limit then we can stop here, no point in doing the extra work
         if(limit != Double.POSITIVE_INFINITY) { // check if there's a limit set
             // if so then reverse engineer the max LCSS distance and replace the limit
@@ -59,87 +63,76 @@ public class LCSSDistance extends MatrixBasedDistanceMeasure {
             limit = (1 - limit) * Math.min(aLength, bLength);
             // is potentially slightly too low, causing *early* early abandon
         }
+        
         // start and end of window
-        int start = 1; // start at 1 because 0th element is filled directly below
+        int start = 0;
+        int j = start;
+        int i = 0;
         double mid;
         int end =  (int) Math.min(bLength - 1, Math.ceil(windowSize));
         int prevEnd;
-        // setup matrix and rows
-        final boolean generateDistanceMatrix = isGenerateDistanceMatrix();
-        final double[][] matrix;
-        double[] row;
-        double[] prevRow = null;
-        if(generateDistanceMatrix) {
-            matrix = new double[aLength][bLength];
-            for(double[] array : matrix) Arrays.fill(array, Double.NEGATIVE_INFINITY);
-            row = matrix[0];
-        } else {
-            matrix = null;
-            row = new double[bLength];
-            prevRow = new double[bLength];
-        }
-        setDistanceMatrix(matrix);
+        double[] row = getRow(i);
+        double[] prevRow;
+        
         // process top left sqaure of mat
-        double min, cost;
-        row[0] = min = cost = approxEqual(a, 0, b, 0) ? 1 : 0;
+        double min = row[j] = approxEqual(a, i, b, j) ? 1 : 0;
+        j++;
         // compute the first row
-        for(int j = start; j <= end; j++) {
-            if(approxEqual(a, 0, b, j)) {
-                cost = 1;
+        for(; j <= end; j++) {
+            if(approxEqual(a, i, b, j)) {
+                row[j] = 1;
             } else {
-                cost = row[j - 1];
+                row[j] = row[j - 1];
             }
-            row[j] = cost;
-            min = Math.min(min, cost);
+            min = Math.min(min, row[j]);
         }
         if(min > limit) return Double.POSITIVE_INFINITY; // quit if beyond limit
+        i++;
+        
         // process remaining rows
-        for(int i = 1; i < aLength; i++) {
+        for(; i < aLength; i++) {
             // reset min for the row
             min = Double.POSITIVE_INFINITY;
+            
             // start, end and mid of window
             prevEnd = end;
             mid = i * lengthRatio;
             start = (int) Math.max(0, Math.floor(mid - windowSize));
+            j = start;
             end = (int) Math.min(bLength - 1, Math.ceil(mid + windowSize));
+            
             // change rows
-            if(generateDistanceMatrix) {
-                row = matrix[i];
-                prevRow = matrix[i - 1];
-            } else {
-                // reuse previous row
-                double[] tmp = row;
-                row = prevRow;
-                prevRow = tmp;
-                // set the top values outside of window to inf
-                Arrays.fill(prevRow, prevEnd + 1, end + 1, Double.NEGATIVE_INFINITY);
-                // set the value left of the window to inf
-                if(start > 0) row[start - 1] = Double.NEGATIVE_INFINITY;
-            }
+            prevRow = row;
+            row = getRow(i);
+            
+            // set the top values outside of window to inf
+            Arrays.fill(prevRow, prevEnd + 1, end + 1, Double.NEGATIVE_INFINITY);
+            // set the value left of the window to inf
+            if(j > 0) row[j - 1] = Double.NEGATIVE_INFINITY;
+            
             // if assessing the left most column then only mapping option is top - not left or topleft
-            if(start == 0) {
-                if(approxEqual(a, i, b, start)) {
-                    cost = 1;
-                } else {
-                    cost = prevRow[start];
-                }
-                row[start++] = cost;
-                min = Math.min(min, cost);
-            }
-            // compute the distance for each cell in the row
-            for(int j = start; j <= end; j++) {
+            if(j == 0) {
                 if(approxEqual(a, i, b, j)) {
-                    cost = prevRow[j - 1] + 1;
+                    row[j] = 1;
                 } else {
-                    cost = Math.max(row[j - 1], Math.max(prevRow[j], prevRow[j - 1]));
+                    row[j] = prevRow[start];
                 }
-                row[j] = cost;
-                min = Math.min(min, cost);
+                min = Math.min(min, row[j++]);
+            }
+            
+            // compute the distance for each cell in the row
+            for(; j <= end; j++) {
+                if(approxEqual(a, i, b, j)) {
+                    row[j] = prevRow[j - 1] + 1;
+                } else {
+                    row[j] = Math.max(row[j - 1], Math.max(prevRow[j], prevRow[j - 1]));
+                }
+                min = Math.min(min, row[j]);
             }
             if(min > limit) return Double.POSITIVE_INFINITY; // quit if beyond limit
         }
         // last value in the current row is the distance
-        return 1d - row[bLength - 1] / Math.min(aLength, bLength);
+        return 1d - row[row.length - 1] / Math.min(aLength, bLength);
     }
 
     @Override
