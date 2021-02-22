7e33e61dae7c4d383a3650b83c1dc494eba03014
Conor Egan <68134729+c-eg@users.noreply.github.com>
2021-02-22 22:21:53 +0000

    JavaDoc comments done

--- src/main/java/tsml/data_containers/TimeSeries.java
+++ src/main/java/tsml/data_containers/TimeSeries.java
@@ -150,6 +150,7 @@ public class TimeSeries implements Iterable<Double> {
      * @return Sliding window of series
      */
     public List<Double> getSlidingWindow(int start, int end) {
+        // TODO: duplicate, remove usages and replace with getVSliceList(start, end)
         return series.subList(start, end);
     }
 
@@ -162,6 +163,7 @@ public class TimeSeries implements Iterable<Double> {
      * @return Sliding window of series
      */
     public double[] getSlidingWindowArray(int start, int end) {
+        // TODO: duplicate, remove usages and replace with getVSliceArray(start, end)
         return series.subList(start, end).stream().mapToDouble(Double::doubleValue).toArray();
     }
 
@@ -197,41 +199,87 @@ public class TimeSeries implements Iterable<Double> {
         return sb.substring(0, sb.length() - 1);
     }
 
-    /** 
-     * @return double[]
+    /**
+     * Returns all values in the series.
+     *
+     * @return values in series
      */
 	public double[] toValueArray() {
 		return getSeries().stream().mapToDouble(Double::doubleValue).toArray();
     }
 
-    public TimeSeries getVSlice(int[] indices) {
-        return new TimeSeries(getVSliceArray(indices));
-    }
-    
-    public TimeSeries getVSlice(int index) {
-	    return getVSlice(new int[] {index});
-    }
-
-    public TimeSeries getVSlice(List<Integer> indices) {
-	    return getVSlice(indices.stream().mapToInt(Integer::intValue).toArray());
+    /**
+     * Returns a new TimeSeries object containing only values at indexes passed.
+     *
+     * @param indexesToKeep from series
+     * @return a new TimeSeries object
+     */
+    public TimeSeries getVSlice(int[] indexesToKeep) {
+        return new TimeSeries(getVSliceArray(indexesToKeep));
     }
 
-    public TimeSeries getVSliceComplement(int index) {
-	    return getVSliceComplement(new int[] {index});
-    }
-    
-    public TimeSeries getVSliceComplement(int[] indices) {
-        return new TimeSeries(getVSliceComplementArray(indices));
+    /**
+     * Returns a new TimeSeries object containing only the value at the index
+     * passed.
+     *
+     * @param indexToKeep from series
+     * @return a new TimeSeries object
+     */
+    public TimeSeries getVSlice(int indexToKeep) {
+	    return getVSlice(new int[] {indexToKeep});
     }
 
-    public TimeSeries getVSliceComplement(List<Integer> indices) {
-        return getVSliceComplement(indices.stream().mapToInt(Integer::intValue).toArray());
+    /**
+     * Returns a new TimeSeries object containing only values at indexes passed.
+     *
+     * @param indexesToKeep from series
+     * @return a new TimeSeries object
+     */
+    public TimeSeries getVSlice(List<Integer> indexesToKeep) {
+	    return getVSlice(indexesToKeep.stream().mapToInt(Integer::intValue).toArray());
     }
-    
-    /** 
-     * this is useful if you want to delete a column/truncate the array, but without modifying the original dataset.
-     * @param indexesToRemove
-     * @return List<Double>
+
+    /**
+     * Returns a new TimeSeries object containing all values apart from index
+     * passed.
+     *
+     * @param indexToRemove from series
+     * @return a new TimeSeries object
+     */
+    public TimeSeries getVSliceComplement(int indexToRemove) {
+	    return getVSliceComplement(new int[] {indexToRemove});
+    }
+
+    /**
+     * Returns a new TimeSeries object containing all values apart from indexes
+     * passed.
+     *
+     * @param indexesToRemove from series
+     * @return a new TimeSeries object
+     */
+    public TimeSeries getVSliceComplement(int[] indexesToRemove) {
+        return new TimeSeries(getVSliceComplementArray(indexesToRemove));
+    }
+
+    /**
+     * Returns a new TimeSeries object containing all values apart from indexes
+     * passed.
+     *
+     * @param indexesToRemove from series
+     * @return a new TimeSeries object
+     */
+    public TimeSeries getVSliceComplement(List<Integer> indexesToRemove) {
+        return getVSliceComplement(indexesToRemove.stream().mapToInt(Integer::intValue).toArray());
+    }
+
+    /**
+     * Returns a list of series containing all values apart from indexes passed.
+     *
+     * This is useful if you want to delete a column/truncate the array, but
+     * without modifying the original dataset.
+     *
+     * @param indexesToRemove from series
+     * @return a list of new series
      */
     public List<Double> getVSliceComplementList(List<Integer> indexesToRemove){
         //if the current index isn't in the removal list, then copy across.
@@ -243,40 +291,62 @@ public class TimeSeries implements Iterable<Double> {
 
         return out;
     }
-    
+
+    /**
+     * Returns a list of series containing all values apart from indexes passed.
+     *
+     * @param indexesToRemove from series
+     * @return a list of new series
+     */
     public List<Double> getVSliceComplementList(int[] indexesToRemove) {
         return getVSliceComplementList(Arrays.stream(indexesToRemove).boxed().collect(Collectors.toList()));
     }
 
-    public List<Double> getVSliceComponentList(int index) {
-        return getVSliceComplementList(new int[] {index});
-    }
-    
-    /** 
-     * @param indexesToKeep
-     * @return double[]
+    /**
+     * Returns a list of series containing all values apart from index passed.
+     *
+     * @param indexToRemove from series
+     * @return a list of new series
      */
-    public double[] getVSliceComplementArray(int[] indexesToKeep){
-        return getVSliceComplementArray(Arrays.stream(indexesToKeep).boxed().collect(Collectors.toList()));
+    public List<Double> getVSliceComponentList(int indexToRemove) {
+        return getVSliceComplementList(new int[] {indexToRemove});
     }
 
-    
-    /** 
-     * @param indexesToRemove
-     * @return double[]
+    /**
+     * Returns an array of series containing all values apart from indexes passed.
+     *
+     * @param indexesToRemove from series
+     * @return an array of new series
+     */
+    public double[] getVSliceComplementArray(int[] indexesToRemove){
+        return getVSliceComplementArray(Arrays.stream(indexesToRemove).boxed().collect(Collectors.toList()));
+    }
+
+    /**
+     * Returns an array of series containing all values apart from indexes passed.
+     *
+     * @param indexesToRemove from series
+     * @return an array of new series
      */
     public double[] getVSliceComplementArray(List<Integer> indexesToRemove){
         return getVSliceComplementList(indexesToRemove).stream().mapToDouble(Double::doubleValue).toArray();
     }
-    
-    public double[] getVSliceComplementArray(int index) {
-        return getVSliceComplementArray(new int[] {index});
+
+    /**
+     * Returns an array of series containing all values apart from index passed.
+     *
+     * @param indexToRemove from series
+     * @return an array of new series
+     */
+    public double[] getVSliceComplementArray(int indexToRemove) {
+        return getVSliceComplementArray(new int[] {indexToRemove});
     }
-    
-    /** 
-     * this is useful if you want to slice a column/truncate the array, but without modifying the original dataset.
-     * @param indexesToKeep
-     * @return List<Double>
+
+    /**
+     * Returns a list of series containing only values at indexes passed.
+     *
+     * @param indexesToKeep from series
+     * @return a list of new series
      */
     public List<Double> getVSliceList(List<Integer> indexesToKeep){
         //if the current index isn't in the removal list, then copy across.
@@ -288,70 +358,133 @@ public class TimeSeries implements Iterable<Double> {
 
         return out;
     }
-    
+
+    /**
+     * Returns a list of series containing only values at indexes passed.
+     *
+     * @param indexesToKeep from series
+     * @return a list of new series
+     */
     public List<Double> getVSliceList(int[] indexesToKeep) {
         return getVSliceList(Arrays.stream(indexesToKeep).boxed().collect(Collectors.toList()));
     }
-    
-    public List<Double> getVSliceList(int index) {
-        return getVSliceList(new int[] {index});
+
+    /**
+     * Returns a list of series containing only the value at index passed.
+     *
+     * @param indexToKeep from series
+     * @return a list of new series
+     */
+    public List<Double> getVSliceList(int indexToKeep) {
+        return getVSliceList(new int[] {indexToKeep});
     }
 
-    public double[] getVSliceArray(int index) {
-        return getVSliceArray(new int[] {index});
-    }
-    
-    /** 
-     * @param indexesToKeep
-     * @return double[]
+    /**
+     * Returns an array of series containing only the value at index passed.
+     *
+     * @param indexToKeep from series
+     * @return a list of new series
      */
-    public double[] getVSliceArray(int[] indexesToKeep){
+    public double[] getVSliceArray(int indexToKeep) {
+        return getVSliceArray(new int[] {indexToKeep});
+    }
+
+    /**
+     * Returns an array of series containing only values at indexes passed.
+     *
+     * @param indexesToKeep from series
+     * @return a list of new series
+     */
+    public double[] getVSliceArray(int[] indexesToKeep) {
         return getVSliceArray(Arrays.stream(indexesToKeep).boxed().collect(Collectors.toList()));
     }
 
-    
-    /** 
-     * @param indexesToKeep
-     * @return double[]
+    /**
+     * Returns an array of series containing only values at indexes passed.
+     *
+     * @param indexesToKeep from series
+     * @return a list of new series
      */
-    public double[] getVSliceArray(List<Integer> indexesToKeep){
+    public double[] getVSliceArray(List<Integer> indexesToKeep) {
         return getVSliceList(indexesToKeep).stream().mapToDouble(Double::doubleValue).toArray();
     }
-    
-    /** 
-     * @param args
-     */
-    public static void main(String[] args) {
-        TimeSeries ts = new TimeSeries(new double[]{1,2,3,4}) ;
-    }
 
+    /**
+     * Returns an iterator, iterating over the series.
+     *
+     * @return series iterator
+     */
     @Override public Iterator<Double> iterator() {
         return series.iterator();
     }
-    
+
+    /**
+     * Returns a list of the portion of the series between the specified start,
+     * inclusive, and end, exclusive.
+     *
+     * @param startInclusive index to start from (inclusive)
+     * @param endExclusive index to end from (exclusive)
+     * @return Sliding window of series
+     */
     public List<Double> getVSliceList(int startInclusive, int endExclusive) {
         return series.subList(startInclusive, endExclusive);
     }
-    
+
+    /**
+     * Returns an array of the portion of the series between the specified start,
+     * inclusive, and end, exclusive.
+     *
+     * @param startInclusive index to start from (inclusive)
+     * @param endExclusive index to end from (exclusive)
+     * @return Sliding window of series
+     */
     public double[] getVSliceArray(int startInclusive, int endExclusive) {
         return getVSliceList(startInclusive, endExclusive).stream().mapToDouble(d -> d).toArray();
     }
-    
+
+    /**
+     * Returns a new TimeSeries object containing a portion of the series between
+     * the specified start, inclusive, and end, exclusive.
+     *
+     * @param startInclusive index to start from (inclusive)
+     * @param endExclusive index to end from (exclusive)
+     * @return Sliding window of series
+     */
     public TimeSeries getVSlice(int startInclusive, int endExclusive) {
         final TimeSeries ts = new TimeSeries();
         ts.series = getVSliceList(startInclusive, endExclusive);
         return ts;
     }
 
-    @Override public boolean equals(final Object o) {
-        if(!(o instanceof TimeSeries)) {
+    /**
+     * Returns whether a TimeSeries object is equal to another based if the series
+     * are exactly the same.
+     *
+     * @param other object
+     * @return true if equal, false if not
+     */
+    @Override
+    public boolean equals(final Object other) {
+        if (!(other instanceof TimeSeries)) {
             return false;
         }
-        final TimeSeries that = (TimeSeries) o;
+        final TimeSeries that = (TimeSeries) other;
         return Objects.equals(series, that.series);
     }
 
+    /**
+     * Returns an int of the hash code based on the series.
+     *
+     * @return hash code
+     */
     @Override public int hashCode() {
         return Objects.hash(series);
+    }
+
+    /**
+     * Example
+     */
+    public static void main(String[] args) {
+        TimeSeries ts = new TimeSeries(new double[]{1,2,3,4});
     }
 }
