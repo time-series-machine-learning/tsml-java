8bed2cbdacb1dfd3a6df93d96f048ba4b5d161b6
George Oastler <goastler4@gmail.com>
2021-03-15 10:54:13 +0000

    Adjusted base transformer

--- src/main/java/tsml/transformers/BaseTrainableTransformer.java
+++ src/main/java/tsml/transformers/BaseTrainableTransformer.java
@@ -1,22 +1,31 @@
 package tsml.transformers;
 
+import tsml.data_containers.TimeSeriesInstances;
+import tsml.data_containers.utilities.Converter;
 import weka.core.Instance;
 import weka.core.Instances;
 
 public abstract class BaseTrainableTransformer implements TrainableTransformer {
 
-    private boolean isFit;
+    protected boolean fitted;
 
-    @Override public void fit(final Instances data) {
-        isFit = true;
+    @Override public void fit(final TimeSeriesInstances data) {
+        fitted = true;
     }
 
     public void reset() {
-        isFit = false;
+        fitted = false;
     }
 
     @Override public boolean isFit() {
-        return isFit;
+        return fitted;
     }
 
+    @Override public Instance transform(final Instance inst) {
+        return Converter.toArff(transform(Converter.fromArff(inst)));
+    }
+
+    @Override public void fit(final Instances data) {
+        fit(Converter.fromArff(data));
+    }
 }
