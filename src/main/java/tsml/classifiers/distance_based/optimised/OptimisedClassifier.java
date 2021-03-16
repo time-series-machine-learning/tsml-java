d554600b4031f391855639ad06e16c0f9f5ca379
George Oastler <goastler4@gmail.com>
2021-03-16 02:16:00 +0000

    iterable optimisation for tuned knns

--- src/main/java/tsml/classifiers/distance_based/optimised/OptimisedClassifier.java
+++ src/main/java/tsml/classifiers/distance_based/optimised/OptimisedClassifier.java
@@ -4,6 +4,7 @@ import evaluation.storage.ClassifierResults;
 import experiments.data.DatasetLoading;
 import tsml.classifiers.TSClassifier;
 import tsml.classifiers.TestTimeContractable;
+import tsml.classifiers.TrainEstimateTimeable;
 import tsml.classifiers.distance_based.distances.dtw.spaces.DTWDistanceSpace;
 import tsml.classifiers.distance_based.utils.classifiers.BaseClassifier;
 import tsml.classifiers.distance_based.utils.classifiers.TimedTest;
@@ -14,6 +15,8 @@ import tsml.classifiers.distance_based.utils.classifiers.contracting.Progressive
 import tsml.classifiers.distance_based.utils.classifiers.contracting.TimedTrain;
 import tsml.classifiers.distance_based.utils.classifiers.results.ResultUtils;
 import tsml.classifiers.distance_based.utils.collections.params.iteration.RandomSearch;
+import tsml.classifiers.distance_based.utils.system.memory.MemoryWatchable;
+import tsml.classifiers.distance_based.utils.system.memory.MemoryWatcher;
 import tsml.classifiers.distance_based.utils.system.random.RandomUtils;
 import tsml.classifiers.distance_based.utils.system.timing.StopWatch;
 import tsml.data_containers.TimeSeriesInstance;
@@ -21,14 +24,17 @@ import tsml.data_containers.TimeSeriesInstances;
 import utilities.ClassifierTools;
 import weka.core.Instances;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
-public class OptClassifier extends BaseClassifier implements Chkpt, ProgressiveBuild, TimedTrain, TimedTest,
-                                                                     ContractedTrain {
+public class OptimisedClassifier extends BaseClassifier implements Chkpt, ProgressiveBuild, TimedTrain, TimedTest,
+                                                                     ContractedTrain, MemoryWatchable,
+                                                                           TrainEstimateTimeable, IterableBuild {
 
     public static void main(String[] args) throws Exception {
         final int seed = 0;
-        final OptClassifier classifier = new OptClassifier();
+        final OptimisedClassifier classifier = new OptimisedClassifier();
         final KnnAgent agent = new KnnAgent();
 //        agent.setParamSpaceBuilder(new EDistanceSpace());
         agent.setParamSpaceBuilder(new DTWDistanceSpace());
@@ -45,25 +51,34 @@ public class OptClassifier extends BaseClassifier implements Chkpt, ProgressiveB
     private Agent agent;
     private Evaluation bestEvaluation;
     private final CheckpointConfig checkpointConfig = new CheckpointConfig();
-    private StopWatch runTimer = new StopWatch();
-    private StopWatch testTimer = new StopWatch();
+    private final StopWatch runTimer = new StopWatch();
+    private final StopWatch testTimer = new StopWatch();
+    private final StopWatch trainEstimateTimer = new StopWatch();
+    private final MemoryWatcher memoryWatcher = new MemoryWatcher();
     private long trainTimeLimit = -1;
     private long testTimeLimit = -1;
     private long longestEvaluationTime;
-
-    @Override public boolean isFullyBuilt() {
-        return agent.hasNext();
+    private TimeSeriesInstances trainData;
+    private boolean explore;
+    
+    public void setTrainData(TimeSeriesInstances trainData) {
+        this.trainData = Objects.requireNonNull(trainData);
     }
 
-    @Override public void buildClassifier(final TimeSeriesInstances trainData) throws Exception {
+    @Override public boolean isFullyBuilt() {
+        return IterableBuild.super.isFullyBuilt();
+    }
+
+    @Override public void beforeBuild() throws Exception {
         long timeStamp = System.nanoTime();
-        runTimer.start(timeStamp);
-        
+        memoryWatcher.start();
+        checkpointConfig.setLogger(getLogger());
+
         if(isRebuild()) {
             // attempt to load from a checkpoint
             if(loadCheckpoint()) {
-                // internals of this object have been changed, so the run timer needs restarting
-                runTimer.start(timeStamp); // start from same time point though, no time missed while dealing with chkp
+                memoryWatcher.start();
+                checkpointConfig.setLogger(getLogger());
             } else {
                 super.buildClassifier(trainData);
                 if(agent == null) {
@@ -73,53 +88,72 @@ public class OptClassifier extends BaseClassifier implements Chkpt, ProgressiveB
                 copyRandomTo(agent);
                 agent.buildAgent(trainData);
                 longestEvaluationTime = 0;
+                runTimer.reset();
+                trainEstimateTimer.reset();
             }
-            
+
         }
+        runTimer.start(timeStamp);
         
-        boolean workDone = false;
+        memoryWatcher.stop();
+        runTimer.stop();
+    }
+
+    @Override public boolean hasNextBuildStep() throws Exception {
+
         // x2 on the longest eval time because we need an extra slot of eval time to recompute the results
         // granted this means we end up re-evaluating the best classifier again, but a) some classifiers like knn bare
         // little cost in doing this and b) it's better to do this than store all the results for every single
         // evaluation and eat a million gigs of ram
-        boolean explore = false;
-        while(agent.hasNext() && insideTrainTimeLimit(getRunTime() + longestEvaluationTime * 2)) {
-            timeStamp = System.nanoTime();
-            final Evaluation evaluation = agent.next();
-            if(explore != evaluation.isExplore()) {
-                explore = !explore;
-                getLogger().info("----");
-            }
-            execute(evaluation, trainData);
-            agent.feedback(evaluation);
-            workDone = true;
-            longestEvaluationTime = Math.max(longestEvaluationTime, System.nanoTime() - timeStamp);
-        }
-        
-        if(workDone || trainResults.getPredClassVals() == null) {
-            final List<Evaluation> bestEvaluations = agent.getBestEvaluations();
-            bestEvaluation = RandomUtils.choice(bestEvaluations, getRandom());
-            // best evaluation may need results recalculating if they're dropped because of memory requirements
-            if(bestEvaluation.getResults() == null) {
-                getLogger().info("----");
-                getLogger().info("recomputing evaluation for best");
-                execute( bestEvaluation, trainData);
-            }
-            trainResults = bestEvaluation.getResults();
-        }
-        
-        runTimer.stop();
+        return agent.hasNext() && insideTrainTimeLimit(getRunTime() + longestEvaluationTime * 2);
+    }
 
+    @Override public void nextBuildStep() throws Exception {
+        runTimer.start();
+        memoryWatcher.stop();
+        
+        final long timeStamp = System.nanoTime();
+        final Evaluation evaluation = agent.next();
+        if(explore != evaluation.isExplore()) {
+            explore = !explore;
+            getLogger().info("----");
+        }
+        execute(evaluation, trainData);
+        agent.feedback(evaluation);
+        longestEvaluationTime = Math.max(longestEvaluationTime, System.nanoTime() - timeStamp);
+        
+        memoryWatcher.stop();
+        runTimer.stop();
+    }
+
+    @Override public void afterBuild() throws Exception {
+        runTimer.start();
+        memoryWatcher.start();
+        
+        final List<Evaluation> bestEvaluations = agent.getBestEvaluations();
+        bestEvaluation = RandomUtils.choice(bestEvaluations, getRandom());
+        // best evaluation may need results recalculating if they're dropped because of memory requirements
+        if(bestEvaluation.getResults() == null) {
+            getLogger().info("----");
+            getLogger().info("recomputing evaluation for best");
+            execute(bestEvaluation, trainData);
+        }
+        trainResults = bestEvaluation.getResults();
+
+        memoryWatcher.stop();
+        runTimer.stop();
         // we do this after the timers have been stopped, etc, otherwise times are inaccurate
         // this sets the classifier name / dataset name / timings / meta in the results
         ResultUtils.setInfo(trainResults, this, trainData);
     }
     
     private void execute(Evaluation evaluation, TimeSeriesInstances trainData) throws Exception {
+        trainEstimateTimer.start();
         final TSClassifier classifier = evaluation.getClassifier();
         final ClassifierResults results = evaluation.getEvaluator().evaluate(classifier, trainData);
         evaluation.setResults(results);
         getLogger().info(evaluation::toStringVerbose);
+        trainEstimateTimer.stop();
     }
 
     @Override public double[] distributionForInstance(final TimeSeriesInstance inst) throws Exception {
@@ -146,7 +180,11 @@ public class OptClassifier extends BaseClassifier implements Chkpt, ProgressiveB
     }
 
     @Override public long getTrainTime() {
-        return getRunTime() - getCheckpointingTime();
+        return getRunTime() - getCheckpointingTime() - getTrainEstimateTime();
+    }
+
+    @Override public long getTrainEstimateTime() {
+        return trainEstimateTimer.elapsedTime();
     }
 
     @Override public long getRunTime() {
@@ -171,5 +209,9 @@ public class OptClassifier extends BaseClassifier implements Chkpt, ProgressiveB
 
     @Override public void setTrainTimeLimit(final long trainTimeLimit) {
         this.trainTimeLimit = trainTimeLimit;
+    }
+
+    @Override public long getMaxMemoryUsage() {
+        return memoryWatcher.getMaxMemoryUsage();
     }
 }
