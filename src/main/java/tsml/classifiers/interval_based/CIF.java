1853e4ca64ac91291c77830fd0d274de75a616da
MatthewMiddlehurst <pfm15hbu@uea.ac.uk>
2021-03-31 13:15:13 +0100

    oob fixes

--- src/main/java/tsml/classifiers/interval_based/CIF.java
+++ src/main/java/tsml/classifiers/interval_based/CIF.java
@@ -515,9 +515,9 @@ public class CIF extends EnhancedAbstractClassifier implements TechnicalInformat
     /**
      * Build the CIF classifier
      * For each base classifier
-     * generate random intervals
-     * do the transfrorms
-     * build the classifier
+     *      generate random intervals
+     *      do the transfrorms
+     *      build the classifier
      *
      * @param data   TimeSeriesInstances data
      * @param result Instances object formatted for transformed data
@@ -715,9 +715,9 @@ public class CIF extends EnhancedAbstractClassifier implements TechnicalInformat
      * Build the CIF classifier using multiple threads.
      * Unable to checkpoint until after the build process while using multiple threads.
      * For each base classifier
-     *     generate random intervals
-     *     do the transfrorms
-     *     build the classifier
+     *      generate random intervals
+     *      do the transfrorms
+     *      build the classifier
      *
      * @param data   TimeSeriesInstances data
      * @param result Instances object formatted for transformed data
@@ -780,8 +780,11 @@ public class CIF extends EnhancedAbstractClassifier implements TechnicalInformat
             long[] predTimes = new long[data.numInstances()];//Dummy variable, need something
             for (int j = 0; j < data.numInstances(); j++) {
                 long predTime = System.nanoTime();
-                for (int k = 0; k < trainDistributions[j].length; k++)
-                    trainDistributions[j][k] /= oobCounts[j];
+                if (oobCounts[j] == 0)
+                    Arrays.fill(trainDistributions[j], 1.0 / trainDistributions[j].length);
+                else
+                    for (int k = 0; k < trainDistributions[j].length; k++)
+                        trainDistributions[j][k] /= oobCounts[j];
                 preds[j] = findIndexOfMax(trainDistributions[j], rand);
                 actuals[j] = data.get(j).getLabelIndex();
                 predTimes[j] = System.nanoTime() - predTime;
