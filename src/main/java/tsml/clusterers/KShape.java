4d7178c78c01cb88373225f1b51be8c93a79cb11
pfm15hbu <m.middlehurst@uea.ac.uk>
2021-08-31 16:28:58 +0100

    kshape fix

--- src/main/java/tsml/clusterers/KShape.java
+++ src/main/java/tsml/clusterers/KShape.java
@@ -91,13 +91,12 @@ public class KShape extends EnhancedAbstractClusterer implements NumberOfCluster
             rand = new Random(seed);
         }
 
-//        assignments = new double[train.numInstances()];
-//        //Randomly assign clusters
-//        for (int i = 0; i < assignments.length; i++) {
-//            assignments[i] = (int) Math.ceil(rand.nextDouble() * k) - 1;
-//        }
+        assignments = new double[train.numInstances()];
+        //Randomly assign clusters
+        for (int i = 0; i < assignments.length; i++) {
+            assignments[i] = (int) Math.ceil(rand.nextDouble() * k) - 1;
+        }
 
-        assignments = new double[] {2,3,1,2,1,1,1,2,2,3,2,3,1,4,1,3,2,3,1,1,4,4,2,3,4,4,1,1,1,4,1,2,4,3,3,2,3,4,1,4,4,3,2,4,1,2,4,2,2,1,1,3,1,2,2,1,3,1,3,3,1,2,3,2,1,3,3,3,4,3,4,1,1,4,2,1,4,2,4,3,4,3,4,2,2,4,2,4,3,3,1,4,2,3,2,1,4,3,1,3,2,3,4,2,4,3,1,4,3,4,1,1,4,3,1,4,4,4,3,1,1,1,1,1,4,3,3,4,1,2,3,4,3,1,4,1,4,2,4,3,3,1,1,1,1,1,1,3,3,1,1,4,3,1,2,3,1,3,4,4,1,2,3,4,1,1,1,2,3,3,2,4,3,2,3,3,3,2,1,2,3,1,4,1,2,4,1,3,3,4,2,1,3,4,4,4,1,1,3,4};
         for (int i = 0; i < assignments.length; i++)
             assignments[i] -= 1;
 
